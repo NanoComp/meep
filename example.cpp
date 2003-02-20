8828cbb12c952bf5d15e3399f9cd1a60ff4e22c7
droundy <droundy@abridgegame.org>
2003-02-20 08:11:01 -0500

    Change example to a fancy band structure example.

--- example.cpp
+++ example.cpp
@@ -40,18 +40,29 @@ double guided_eps(double r, double z) {
   return 1.6*1.6;             // in the low dielectric
 }
 
+static double rsource = 0.15;
+
 double source_sharp2(double r) {
   if (r == 0) return 0;
-  double dr = r - 1.2; //.5;
+  double dr = r - 0.6;
   double sig = 0.15;
   return exp(-dr*dr/(sig*sig));
 }
 
 double source_sharp(double r) {
   if (r == 0) return 0;
-  double dr = r - 0.3;
+  double dr = r - rsource;
   double sig = 0.15;
   return exp(-dr*dr/(sig*sig));
+}
+
+void add_clever_sources(fields &f, double fmin, double fmax, double r) {
+  rsource = r;
+  const double A = 0.5;
+  f.add_ep_source(0.33*fmin, 0.13*fmin/fmax, 0.0, 8.0*(1+0*fmin/fmax*A), 0.0, source_sharp);
+  f.add_ez_source(0.33*fmin, 0.13*fmin/fmax, 0.0, 8.0*(1+1*fmin/fmax*A), 0.0, source_sharp);
+  f.add_hp_source(0.33*fmin, 0.13*fmin/fmax, 0.0, 8.0*(1+2*fmin/fmax*A), 0.0, source_sharp);
+  f.add_hz_source(0.33*fmin, 0.13*fmin/fmax, 0.0, 8.0*(1+3*fmin/fmax*A), 0.0, source_sharp);
 }
 
 int main() {
@@ -62,27 +73,28 @@ int main() {
   rad = 10;
   int m=1;
   double k = 0.0;
-  mat ma(guided_eps, 3.5, 3.0, rad);
-  ma.use_pml(8,8);
+  int ttot = 8000*rad;
+
+  mat ma(guided_eps, 1.0, 0.0, rad);
+  //ma.use_pml(8,8);
   ma.output_slices("example");
   for (m=1;m<2 && !stopnow;m++) {
     for (k=0.3;k<0.31 && !stopnow;k+=0.1) {
       printf("Working on k of %g and m = %d with a=%d...\n", k, m, rad);
       fields f(&ma, m);
-      //f.use_bloch(k);
+      f.use_bloch(k);
       //f.ep[0][14] = 1;
-      //f.add_er_source(0.35 , 0.3, 0.0, 3.0, 0.0*rad, source_sharp);
-      //f.add_er_source(0.38 , 0.3, 0.0, 3.0, 0.0*rad, source_sharp2);
-      //f.add_ep_source(0.13 , 0.5, 0.0, 5.0, 2.0*rad, source_sharp);
-      //f.add_ep_source(0.12 , 0.5, 0.0, 5.0, 2.0*rad, source_sharp2);
-      //f.add_ez_source(0.125, 0.5, 0.0, 5.0, 2.0*rad, source_sharp);
-      f.add_ez_source(0.135, 0.51, 0.0, 5.0, 2.0*rad, source_sharp2);
+      add_clever_sources(f, 0.30, 2.5, 0.15);
+      add_clever_sources(f, 0.31, 2.5, 0.6);
+      add_clever_sources(f, 0.29, 2.5, 0.45);
+      f.prepare_for_bands(0, ttot, 2.5, 20);
+//f.add_ep_source(0.11 , 0.02,  0.0, 8.0, 0.0*rad, source_sharp);
+//      f.add_ep_source(0.12 , 0.02,  0.0, 8.0, 0.0*rad, source_sharp2);
+//      f.add_ez_source(0.125, 0.02,  0.0, 8.0, 0.0*rad, source_sharp);
+//      f.add_ez_source(0.115, 0.021, 0.0, 8.0, 0.0*rad, source_sharp2);
 
       //f.set_frequency_range(0.0,0.4, ((double) f.a) / (c * ((double)ttot)) );
       //f.add_zfluxplane(0,(int)(5*rad),80);
-
-      int ttot = 3000*rad;
-      ttot = 140*rad;
       
       for (int t=0; t < ttot+1 && !stopnow; t++) {
         if (t % (1000*rad) == 0 && t > 137*rad) {
@@ -91,12 +103,13 @@ int main() {
           printf("energy is %lg\n", f.total_energy()/3.66e-6);
           //printf("flux is %lg\n", f.zflux(0,22,83));
         }
-        //f.record_bands();
+        f.record_bands();
+        f.output_point(ban, 0, 0, "point");
         f.step();
         f.dft_flux();
       }
       //f.fluxw_output(sumwflux, "sumwflux");
-      //f.output_bands(ban, "band", 10);
+      f.output_bands(ban, "band", 15);
     }
   }
   fclose(ban);
