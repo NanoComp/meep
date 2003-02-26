a5e918b11f5d7a0f5060adf3e4a9d221ee01fe1b
droundy <droundy@civet.berkeley.edu>
2003-02-26 05:52:05 -0500

    Add a script to call autoconf, etc.

---
+++ autogen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+test -r /usr/share/misc/config.sub && \
+   cp -f /usr/share/misc/config.sub config.sub
+test -r /usr/share/misc/config.guess && \
+   cp -f /usr/share/misc/config.guess config.guess
+autoconf
