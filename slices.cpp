e45db7eeb45d5ec051591cca20f75161dfeda822
droundy <droundy@abridgegame.org>
2003-08-27 08:11:32 -0400

    print slices with lots more digits.

--- slices.cpp
+++ slices.cpp
@@ -73,7 +73,7 @@ static void output_slice(component m, const double *f, const volume &v,
   for (int i=0;i<v.ntot();i++) {
     if (what.contains(v.loc(m,i))) {
       v.loc(m,i).print(out);
-      fprintf(out, "\t%lg\n", f[i]);
+      fprintf(out, "\t%.17lg\n", f[i]);
     }
   }
   fclose(out);
