df214917cba3ad3c17841de864f6b7a921ee0263
droundy <droundy@abridgegame.org>
2003-09-03 13:34:05 -0400

    add good flux tests.

--- tests/flux.cpp
+++ tests/flux.cpp
@@ -24,6 +24,16 @@ double one(const vec &) { return 1.0; }
 static double width = 20.0;
 double bump(const vec &v) { return (fabs(v.z()-50.0) > width)?1.0:12.0; }
 
+double cavity(const vec &v) {
+  const double zz = fabs(v.z() - 7.5);
+  if (zz > 4.0) return 1.0;
+  if (zz < 1.0) return 1.0;
+  double norm = zz;
+  while (norm > 1.0) norm -= 1.0;
+  if (norm > 0.3) return 1.0;
+  return 12.0;
+}
+
 int compare(double a, double b, double eps, const char *n) {
   if (fabs(a-b) > fabs(b)*eps) {
     printf("%s differs by\t%lg out of\t%lg\n", n, a-b, b);
@@ -38,7 +48,6 @@ int flux_1d(const double zmax,
                   double eps(const vec &)) {
   const double a = 10.0;
   const double gridpts = a*zmax;
-  const double ttot = 10.0 + 1e5/zmax;
 
   volume v = volone(zmax,a);
   mat ma(v, eps);
@@ -47,24 +56,80 @@ int flux_1d(const double zmax,
 
   fields f(&ma);
   f.use_real_fields();
-  f.add_point_source(Ex, 0.7, 2.5, 0.0, 3.0, vec(zmax/2+0.3), 1.0);
+  f.add_point_source(Ex, 0.25, 3.5, 0.0, 8.0, vec(zmax/6+0.3), 1.0);
   flux_plane *left = f.add_flux_plane(vec(zmax/3.0), vec(zmax/3.0));
   flux_plane *right = f.add_flux_plane(vec(zmax*2.0/3.0), vec(zmax*2.0/3.0));
 
-  while (f.time() <= f.find_last_source()) f.step();
+  const double ttot = min(10.0 + 1e5/zmax,f.find_last_source());
 
+  f.step();
   volume mid = volone(zmax/3,a);
   mid.origin = vec(zmax/3);
-  double flux_energy=0.0;
+  double flux_left=0.0, flux_right=0.0;
   double delta_energy = f.energy_in_box(mid);
   while (f.time() < ttot) {
     f.step();
-    flux_energy += (c/a)*(right->flux() - left->flux());
+    flux_left  +=  (c/a)*left->flux();
+    flux_right +=  (c/a)*right->flux();
   }
   delta_energy -= f.energy_in_box(mid);
-  master_printf("  Energy change:  \t%lg\n  Integrated flux:\t%lg\n  Ratio:\t%lg\n",
-                delta_energy, flux_energy, flux_energy/delta_energy);
-  return compare(delta_energy, flux_energy, 0.15, "Flux");
+  const double del = flux_left;
+  const double der = flux_right - delta_energy;
+  master_printf("  Delta E:\t%lg\n  Flux left:\t%lg\n  Flux right:\t%lg\n  Ratio:\t%lg\n",
+                delta_energy, del, der, del/der);
+  return compare(del, der, 0.05, "Flux");
+}
+
+double oned_flux(const fields &f, const vec &loc) {
+  monitor_point p;
+  f.get_point(&p, loc);
+  master_printf("Ex is %lg\n", real(p.get_component(Ex)));
+  master_printf("Hy is %lg\n", real(p.get_component(Hy)));
+  return (real(p.get_component(Ex))*real(p.get_component(Hy)) +
+          imag(p.get_component(Ex))*imag(p.get_component(Hy)))
+    *(1.0/pi/8/c);
+}
+
+int cavity_1d(const double boxwidth, const double timewait,
+              double eps(const vec &)) {
+  const double zmax = 15.0;
+  const double a = 10.0;
+  const double gridpts = a*zmax;
+
+  volume v = volone(zmax,a);
+  mat ma(v, eps);
+  ma.use_pml_left(2.0);
+  ma.use_pml_right(2.0);
+
+  fields f(&ma);
+  f.use_real_fields();
+  f.add_point_source(Ex, 0.25, 4.5, 0.0, 8.0, vec(zmax/6+0.3), 1.0e2);
+  flux_plane *left  = f.add_flux_plane(vec(zmax*.5-boxwidth),
+                                       vec(zmax*.5-boxwidth));
+  flux_plane *right = f.add_flux_plane(vec(zmax*.5+boxwidth),
+                                       vec(zmax*.5+boxwidth));
+  volume mid = volone(2*boxwidth,a);
+  mid.origin = vec(zmax*.5-boxwidth-0.25/a);
+
+  while (f.time() < f.find_last_source()) f.step();
+  const double ttot = f.time() + timewait;
+  double flux_left=0.0, flux_right=0.0;
+  const double start_energy = f.energy_in_box(mid);
+  master_printf("  Energy starts at\t%lg\n", start_energy);
+  while (f.time() < ttot) {
+    f.step();
+    flux_left  +=  (c/a)*left->flux();
+    flux_right +=  (c/a)*right->flux();
+  }
+  const double delta_energy = start_energy - f.energy_in_box(mid);
+  const double defl = flux_right - flux_left;
+  master_printf("  Delta E:         \t%lg\n  Integrated Flux:\t%lg\n",
+                delta_energy, defl);
+  master_printf("  Ratio:         \t%lg\n", delta_energy/defl);
+  master_printf("  Fractional error:\t%lg\n",
+                (delta_energy - defl)/start_energy);
+  return compare(start_energy - delta_energy,
+                 start_energy - defl, 0.001, "Flux");
 }
 
 void attempt(const char *name, int allright) {
@@ -76,12 +141,16 @@ int main(int argc, char **argv) {
   initialize(argc, argv);
   master_printf("Trying out the fluxes...\n");
 
+  attempt("Cavity 1D 3.01 73", cavity_1d(3.01, 73.0, cavity));
+  attempt("Cavity 1D 5.0   1", cavity_1d(5.0, 1.0, cavity));
+  attempt("Cavity 1D 3.85 55", cavity_1d(3.85, 55.0, cavity));
+
   width = 20.0;
-  attempt("Flux 1D 100", flux_1d(100.0, bump));
+  attempt("Flux 1D 20", flux_1d(100.0, bump));
   width = 10.0;
-  attempt("Flux 1D 100", flux_1d(100.0, bump));
+  attempt("Flux 1D 10", flux_1d(100.0, bump));
   width = 300.0;
-  attempt("Flux 1D 100", flux_1d(100.0, bump));
+  attempt("Flux 1D 300", flux_1d(100.0, bump));
 
   finished();
   exit(0);
