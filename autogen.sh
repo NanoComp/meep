4f68d736ed4cfd38319ac56d03e101b93827ac9b
stevenj <stevenj@fftw.org>
2003-12-08 15:33:50 -0500

    more verbose darcs_test

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
+echo "arg: $1"
+
 touch hsrc/.depend
 
-autoreconf --verbose --install --symlink
+if test "x$1" = "x--verbose"; then
+    verbose=yes
+else
+    verbose=no
+fi
 
-./configure --enable-maintainer-mode
+if test $verbose = yes; then
+    autoreconf --verbose --install --symlink
+else
+    autoreconf --install --symlink
+fi
+
+config=good # hackery so darcs_test still outputs config.log w/failed configure
+
+./configure --enable-maintainer-mode || config=bad
+
+if test $verbose = yes; then
+    cat config.log
+fi
+
+test $config = bad && exit 1
 
 (cd hsrc; make depend)
 
