35f3ba45a42d0008ed5fee2ab3b8dade40a41a4a
stevenj <stevenj@fftw.org>
2005-05-30 22:34:10 -0400

    whoops

--- src/bicgstab.cpp
+++ src/bicgstab.cpp
@@ -9,7 +9,7 @@ namespace meep {
 
 #ifdef HAVE_CBLAS_DDOT
    extern "C" double cblas_ddot(int, const double*, int, const double*, int);
-#  define dot(n, x, y) cblas_ddot(n, x, 1, y, 1)
+#  define dot(n, x, y) sum_to_all(cblas_ddot(n, x, 1, y, 1))
 #else
 static double dot(int n, const double *x, const double *y)
 {
