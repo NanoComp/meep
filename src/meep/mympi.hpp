8c44fce9e706a8b9cc92369f3400af8e4a31f45e
stevenj <stevenj@fftw.org>
2007-11-13 15:13:59 -0500

    update copyright year to 2007

--- src/meep/mympi.hpp
+++ src/meep/mympi.hpp
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-/* Copyright (C) 2006 Massachusetts Institute of Technology
+/* Copyright (C) 2005-2007 Massachusetts Institute of Technology
 %
 %  This program is free software; you can redistribute it and/or modify
 %  it under the terms of the GNU General Public License as published by
