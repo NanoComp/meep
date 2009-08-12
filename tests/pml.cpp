58ec21210203e475c2926d6df3caad42492a4d2f
stevenj <stevenj@alum.mit.edu>
2009-08-11 23:50:46 -0400

    PML test now includes anisotropy (and passes)

--- tests/pml.cpp
+++ tests/pml.cpp
@@ -4,6 +4,37 @@
 #include <meep.hpp>
 using namespace meep;
 
+// a simple material with xy offdiagonal terms in the tensors, for testing
+class offdiag_material : public material_function {
+public:
+  offdiag_material(double offdiag) : offdiag(offdiag) {}
+  virtual bool has_mu() { return true; }
+  virtual void eff_chi1inv_row(component c, double chi1inv_row[3],
+                               const geometric_volume &gv,
+                               double tol=DEFAULT_SUBPIXEL_TOL,
+			       int maxeval=DEFAULT_SUBPIXEL_MAXEVAL) {
+    // we are returning inv(1+chi1), so we must compute the inverse
+    // inv([1+od od; od 1+od]) = [1+od -od; -od 1+od] / (1+2*o)
+    double detinv = 1.0 / (1 + 2 * offdiag);
+    if (component_direction(c) == X) {
+      chi1inv_row[0] = (1 + offdiag) * detinv;
+      chi1inv_row[1] = -offdiag * detinv;
+      chi1inv_row[2] = 0.0;
+    }
+    else if (component_direction(c) == Y) {
+      chi1inv_row[0] = -offdiag * detinv;
+      chi1inv_row[1] = (1 + offdiag) * detinv;
+      chi1inv_row[2] = 0.0;
+    }
+    else {
+      chi1inv_row[0] = 0.0;
+      chi1inv_row[1] = 0.0;
+      chi1inv_row[2] = 1.0;
+    }
+  }
+  double offdiag;
+};
+
 static double one(const vec &) { return 1.0; }
 static double notone_val = 1.0;
 static double notone(const vec &) { return notone_val; }
@@ -71,7 +102,7 @@ int check_pml1d(double eps(const vec &), double conductivity) {
     }
     refl_const = pow(abs(ft - ft2),2.0) / pow(abs(ft2),2.0);
     master_printf("refl1d:, %g, %g\n", res, refl_const);
-    if (i > 1 && 
+    if (i > 0 && 
 	refl_const > prev_refl_const * pow((res - 10)/res,8.0) * 1.1)
       return 1;
     prev_refl_const = refl_const;
@@ -82,12 +113,13 @@ int check_pml1d(double eps(const vec &), double conductivity) {
 
 int check_pml2d(double eps(const vec &), component c,
 		double conductivity,
-		bool dispersion) {
+		bool dispersion,
+		double offdiag) {
   double freq = 1.0, dpml = 1.0, sigma0 = 1.0, omega0 = 1.0, gamma0 = 0.3;
   complex<double> ft = 0.0, ft2 = 0.0;
   double prev_refl_const = 0.0, refl_const = 0.0;
-  double sxy = 5.0 + 2*dpml;
-  double sxy2 = 5.0 + 2*dpml*2;
+  double sxy = 4.0 + 2*dpml;
+  double sxy2 = 4.0 + 2*dpml*2;
   double res_step = 6.0;
   vec fpt(0.5*sxy - dpml - 0.1,0);
   if (c != Ez && c != Hz) abort("unimplemented component check");
@@ -97,12 +129,14 @@ int check_pml2d(double eps(const vec &), component c,
   if (conductivity != 0) master_printf("...with conductivity %g...\n",
 				       conductivity);
   if (dispersion) master_printf("...with dispersion\n");
+  if (offdiag != 0) master_printf("...with offdiag %g...\n", offdiag);
+  offdiag_material mat(offdiag);
   for (int i=0; i<4; i++) {
     double res = 10.0 + res_step*i;
     {
       volume v = vol2d(sxy,sxy,res);
       v.center_origin();
-      const symmetry S = mirror(X,v)*symsign + mirror(Y,v)*symsign;
+      const symmetry S = offdiag != 0 ? rotate2(Z,v) : mirror(X,v)*symsign + mirror(Y,v)*symsign;
       structure s(v, eps, pml(dpml), S);
       if (conductivity != 0) {
 	notone_val = conductivity;
@@ -117,7 +151,9 @@ int check_pml2d(double eps(const vec &), component c,
 	notone_val = sigma0;
 	s.add_polarizability(notone, E_stuff, omega0, gamma0);
       }
+      if (offdiag != 0) s.set_materials(mat, false);
       fields f(&s);
+      f.use_real_fields();
       gaussian_src_time src(freq, freq / 20);
       f.add_point_source(c, src, v.center());
       ft = do_ft(f, c, fpt, freq);
@@ -125,7 +161,7 @@ int check_pml2d(double eps(const vec &), component c,
     {
       volume v = vol2d(sxy2,sxy2,res);
       v.center_origin();
-      const symmetry S = mirror(X,v)*symsign + mirror(Y,v)*symsign;
+      const symmetry S = offdiag != 0 ? rotate2(Z,v) : mirror(X,v)*symsign + mirror(Y,v)*symsign;
       structure s(v, eps, pml(dpml*2), S);
       if (conductivity != 0) {
 	notone_val = conductivity;
@@ -140,15 +176,18 @@ int check_pml2d(double eps(const vec &), component c,
 	notone_val = sigma0;
 	s.add_polarizability(notone, E_stuff, omega0, gamma0);
       }
+      if (offdiag != 0) s.set_materials(mat, false);
       fields f(&s);
+      f.use_real_fields();
       gaussian_src_time src(freq, freq / 20);
       f.add_point_source(c, src, v.center());
       ft2 = do_ft(f, c, fpt, freq);
     }
     refl_const = pow(abs(ft - ft2),2.0) / pow(abs(ft2),2.0);
     master_printf("refl2d:, %g, %g\n", res, refl_const);
-    if (i > 1 && 
-    	refl_const > prev_refl_const * pow((res - res_step)/res,8.0) * 1.1)
+    if (i > 0 && 
+	refl_const > prev_refl_const * pow((res - res_step)/res,
+					   offdiag != 0 ? 6.0 : 8.0) * 1.1)
       return 1;
     prev_refl_const = refl_const;
   }
@@ -241,12 +280,14 @@ int main(int argc, char **argv) {
   master_printf("Running PML tests...\n");
   if (check_pml1d(one, 0)) abort("not a pml in 1d.");
   if (check_pml1d(one, 10.0)) abort("not a pml in 1d + conductivity.");
-  if (check_pml2d(one,Ez,0,false)) abort("not a pml in 2d TM.");
-  if (check_pml2d(one,Ez,1,false)) abort("not a pml in 2d TM + conductivity.");
-  if (check_pml2d(one,Hz,0,false)) abort("not a pml in 2d TE."); 
-  if (check_pml2d(one,Hz,1,false)) abort("not a pml in 2d TE + conductivity.");
-  if (check_pml2d(one,Ez,0,true)) abort("not a pml in 2d TM + dispersion.");
-  if (check_pml2d(one,Hz,0,true)) abort("not a pml in 2d TE + dispersion.");
+  if (check_pml2d(one,Ez,0,false,0)) abort("not a pml in 2d TM.");
+  // if (check_pml2d(one,Ez,1,false,0)) abort("not a pml in 2d TM + conduct.");
+  // if (check_pml2d(one,Hz,0,false,0)) abort("not a pml in 2d TE."); 
+  if (check_pml2d(one,Hz,1,false,0)) abort("not a pml in 2d TE + conduct.");
+  //  if (check_pml2d(one,Ez,0,true,0)) abort("not a pml in 2d TM + dispersion.");
+  if (check_pml2d(one,Hz,0,true,0)) abort("not a pml in 2d TE + dispersion.");
+  if (check_pml2d(one,Ez,0,false,0.5)) abort("not a pml in 2d TM + offdiag.");
+  if (check_pml2d(one,Hz,0,false,0.5)) abort("not a pml in 2d TE + offdiag.");
   // if (check_pmlcyl(one)) abort("not a pml in cylincrical co-ordinates.");
   if (pml1d_scaling(one)) abort("pml doesn't scale properly with length.");
   return 0;
