91cf18133efb67a18ba1f3ad41cce244b849c01b
stevenj <stevenj@alum.mit.edu>
2012-06-27 10:26:21 -0400

    whoops

--- libctl/meep-ctl-swig.hpp
+++ libctl/meep-ctl-swig.hpp
@@ -27,6 +27,7 @@ ctlio::cvector3_list do_harminv(ctlio::cnumber_list vals, double dt,
 
 ctlio::number_list dft_flux_flux(meep::dft_flux *f);
 ctlio::number_list dft_force_force(meep::dft_force *f);
+ctlio::number_list dft_ldos_ldos(meep::dft_ldos *f);
 
 ctlio::cnumber_list make_casimir_g(double T, double dt, double sigma, meep::field_type ft,
 				   complex<double> (*eps_func)(complex<double> omega) = 0,
