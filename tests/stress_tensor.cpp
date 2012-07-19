72c7c67615f41a3380e4e2be8fe743de13a61dd5
stevenj <stevenj@alum.mit.edu>
2012-07-19 19:00:20 -0400

    fixed stress tensor test (volume list takes a component,not a direction) and added to test suite

--- tests/stress_tensor.cpp
+++ tests/stress_tensor.cpp
@@ -20,13 +20,15 @@ int main(int argc, char **argv) {
   quiet = true;
   grid_volume gv = vol3d(sx+2*dpml,sy+2*dpml,0,res);
   gv.center_origin();
-  const symmetry S = mirror(X,gv);
+  const symmetry S = mirror(X,gv) - mirror(Y,gv);
   structure s(gv, two_waveguides, pml(dpml,X)+pml(dpml,Y), S);
   s.set_epsilon(two_waveguides);
   fields f(&s);
   f.use_bloch(vec(0,0,0.5));
   f.add_point_source(Ey, 0.22, 0.06, 0.0, 4.0, vec(0.5*(d+sw),0,0));
   f.add_point_source(Ey, 0.22, 0.06, 0.0, 4.0, vec(-0.5*(d+sw),0,0));
+
+#if 0
   double T = f.last_source_time();
   int iT = T / f.dt;
   while (f.t < iT) {
@@ -52,18 +54,25 @@ int main(int argc, char **argv) {
   master_printf("harminv found %d modes\n",num);
   for (int i=0;i<num;i++)
     master_printf("freq %d is %0.6g, %0.6g\n",i,freqs_re[i],freqs_im[i]);
+  double freq = freqs_re[0];
+  double T3 = T + T2;
+#else
+  double freq = 0.220847; // hard-code since we are testing force, not harminv
+  double T3 = f.last_source_time() + 300;
+#endif
 
   double dpad = 0.1;
   volume box(vec(0.5*d-dpad,-0.5*sw-dpad,0),vec(0.5*d+sw+dpad,0.5*sw+dpad,0));
-  dft_flux fluxR = f.add_dft_flux_plane(box, freqs_re[0], freqs_re[0], 1);
+  dft_flux fluxR = f.add_dft_flux_plane(box, freq, freq, 1);
   dpad = 0.02;
-  volume_list line(volume(vec(0.5*d-dpad,-0.5*sy,0),vec(0.5*d-dpad,0.5*sy,0)),X);
-  dft_force forceR = f.add_dft_force(&line, freqs_re[0], freqs_re[0], 1);
+  volume_list line(volume(vec(0.5*d-dpad,-0.5*sy,0),vec(0.5*d-dpad,0.5*sy,0)),
+		   Sx);
+  dft_force forceR = f.add_dft_force(&line, freq, freq, 1);
   f.zero_fields();
   f.t = 0;
-  while (f.time() < T + T2) {
+  while (f.time() < T3) {
     if (f.t % 2000 == 0)
-      master_printf("%g%% done with flux and force\n", f.time()/(T+T2) * 100);
+      master_printf("%g%% done with flux and force\n", f.time()/T3 * 100);
     f.step();
   }
   double *fl  = fluxR.flux();
