b68bc6cbb87eae83a9e39d95132aa51777460566
Steven G. Johnson <stevenj@alum.mit.edu>
2014-03-28 17:26:12 -0400

    prepare for 1.2.1 release

--- src/meep/mympi.hpp
+++ src/meep/mympi.hpp
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-/* Copyright (C) 2005-2012 Massachusetts Institute of Technology
+/* Copyright (C) 2005-2014 Massachusetts Institute of Technology
 %
 %  This program is free software; you can redistribute it and/or modify
 %  it under the terms of the GNU General Public License as published by
--- src/random.cpp
+++ src/random.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2005-2012 Massachusetts Institute of Technology.
+/* Copyright (C) 2005-2014 Massachusetts Institute of Technology.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
