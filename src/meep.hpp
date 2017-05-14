d3dba9e0990f7e2ac05baf97253fc4aaf2d05b2b
Homer Reid <homer@homerreid.com>
2017-05-14 11:02:55 -0400

    updates

--- src/meep.hpp
+++ src/meep.hpp
@@ -14,8 +14,8 @@
 %  along with this program; if not, write to the Free Software Foundation,
 %  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
-#ifndef MEEP_H
-#define MEEP_H
+#ifndef MPSESSION_H
+#define MPSESSION_H
 
 #include <stdio.h>
 #include <math.h>
