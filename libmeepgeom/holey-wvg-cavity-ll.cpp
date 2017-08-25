09e1bbe9ecb1da596cc50bd42fda59c3eb9bc3ea
Homer Reid <homer@homerreid.com>
2017-08-24 20:24:51 -0400

    updates to libmeepgeom/holey-wvg-cavity-ll.cpp

--- libmeepgeom/holey-wvg-cavity-ll.cpp
+++ libmeepgeom/holey-wvg-cavity-ll.cpp
@@ -42,6 +42,30 @@ vector3 v3(double x, double y=0.0, double z=0.0)
 }
 
 /***************************************************************/
+/***************************************************************/
+/***************************************************************/
+void run_harminv(fields f, std::vector<cdouble> field_data, double fcen, double df)
+{
+  #define MAXBANDS 100
+  cdouble amp[MAXBANDS];
+  double freq_re[MAXBANDS];
+  double freq_im[MAXBANDS];
+  double err[MAXBANDS];
+  master_printf("starting do_harminv...\n");
+  int bands=do_harminv( &field_data[0], field_data.size(), f.dt,
+                        fcen-0.5*df, fcen+0.5*df, MAXBANDS,
+                        amp, freq_re, freq_im, err);
+
+  master_printf("harminv0: | real(freq) | imag(freq)  |     Q      |  abs(amp)  |          amp          | err\n");
+  master_printf("--------------------------------------------------------------------------------------------\n");
+  for(int nb=0; nb<bands; nb++)
+   master_printf("harminv0: | %.4e | %+.4e | %.4e | %.4e | {%+.2e,%+.2e} | %.1e \n",
+                  freq_re[nb], freq_im[nb], 0.5*freq_re[nb]/freq_im[nb], 
+                  abs(amp[nb]), real(amp[nb]), imag(amp[nb]), err[nb]);
+
+}
+
+/***************************************************************/
 /* dummy material function needed to pass to structure( )      */
 /* constructor as a placeholder before we can call             */
 /* set_materials_from_geometry                                 */
@@ -58,8 +82,8 @@ void holey_wvg_cavity(bool compute_modes=false)
           compute_modes ? "resonant modes" : "transmission spectrum");
 
   // Some parameters to describe the geometry:
-  double Eps=13.0;   // dielectric constant of waveguide
-  double w=1.0;      // width of waveguide
+  double eps=13.0;   // dielectric constant of waveguide
+  double w=1.2;      // width of waveguide
   double r=0.36;     // radius of holes
   double d=1.4;      // defect spacing (ordinary spacing = 1)
   int N=3;           // number of holes on either side of defect
@@ -68,16 +92,6 @@ void holey_wvg_cavity(bool compute_modes=false)
   double sy=6.0;     // size of cell in y direction (perpendicular to wvg.)
   double pad=2.0;    // padding between last hole and PML edge
   double dpml=1.0;   // PML thickness
-
-  // compute-mode == true --> 
-  //  (set! symmetries
-  //   (list (make mirror-sym (direction Y) (phase -1))
-  //         (make mirror-sym (direction X) (phase -1))))
-  // compute-mode == false -->
-  // (set! symmetries (list (make mirror-sym (direction Y) (phase -1))
-  symmetry sym = -mirror(Y,gv);
-  if (compute_modes)
-   sym += -mirror(X,gv);
 
   // (define sx (+ (* 2 (+ pad dpml N)) d -1))
   // (set! geometry-lattice (make lattice (size sx sy no-size)))
@@ -90,6 +104,15 @@ void holey_wvg_cavity(bool compute_modes=false)
   geometry_lattice.size.z=0.0;
   grid_volume gv = voltwo(sx, sy, resolution);
   gv.center_origin();
+
+  // compute-mode == true --> 
+  //  (set! symmetries
+  //   (list (make mirror-sym (direction Y) (phase -1))
+  //         (make mirror-sym (direction X) (phase -1))))
+  // compute-mode == false -->
+  // (set! symmetries (list (make mirror-sym (direction Y) (phase -1))
+  symmetry sym = compute_modes ? -mirror(X,gv) - mirror(Y,gv) : -mirror(Y,gv);
+
   structure the_structure(gv, dummy_eps, pml(dpml), sym);
 
   // (set! geometry
@@ -102,24 +125,29 @@ void holey_wvg_cavity(bool compute_modes=false)
   // (geometric-object-duplicates (vector3 -1 0) 0 (- N 1)
   // (make cylinder (center (/ d -2) 0) (radius r) (height infinity)
   // (material air)))))
+  material_type vacuum     = meep_geom::vacuum;
   material_type dielectric = meep_geom::make_dielectric(eps);
   geometric_object objects[7];
   vector3 origin = v3(0.0,   0.0,  0.0);
   vector3 xhat   = v3(1.0,   0.0,  0.0);
   vector3 yhat   = v3(0.0,   1.0,  0.0);
   vector3 zhat   = v3(0.0,   0.0,  1.0);
-  vector3 center = v3(0.5*d, 0.0,  0.0);
-  vector3 delta  = v3(1.0,   0.0,  0.0);
   vector3 size   = v3(ENORMOUS, w, ENORMOUS);
+  double x0      = 0.5*d;
+  double deltax  = 1.0;
+  double height  = ENORMOUS;
 
   objects[0] = make_block(dielectric, origin, xhat, yhat, zhat, size);
-  objects[1] = make_cylinder(vacuum, center+0*delta, r, height, zhat);
-  objects[2] = make_cylinder(vacuum, center+1*delta, r, height, zhat);
-  objects[3] = make_cylinder(vacuum, center+2*delta, r, height, zhat);
-  objects[4] = make_cylinder(vacuum, -center-0*delta, r, height, zhat);
-  objects[5] = make_cylinder(vacuum, -center-1*delta, r, height, zhat);
-  objects[6] = make_cylinder(vacuum, -center-2*delta, r, height, zhat);
-  geometric_object_list g={ 7, objects };
+  int no=1;
+  for(int n=0; n<N; n++)
+   { vector3 center=v3(x0 + n*deltax,  0.0, 0.0);
+     objects[no++] = make_cylinder(vacuum, center, r, height, zhat);
+   };
+  for(int n=0; n<N; n++)
+   { vector3 center=v3(-x0 - n*deltax,  0.0, 0.0);
+     objects[no++] = make_cylinder(vacuum, center, r, height, zhat);
+   };
+  geometric_object_list g={ no, objects };
   meep_geom::set_materials_from_geometry(&the_structure, g);
   fields f(&the_structure);
 
@@ -136,25 +164,34 @@ void holey_wvg_cavity(bool compute_modes=false)
      // 		(component Hz) (center 0 0))))
      gaussian_src_time src(fcen, df);
      component src_cmpt = Hz;
-     f.add_point_source(src_cmpt, src, origin);
+     f.add_point_source(src_cmpt, src, vec(0.0, 0.0));
 
      // (run-sources+ 400
      //    (at-beginning output-epsilon)
      //    (after-sources (harminv Hz (vector3 0) fcen df)))
-     f.output_hdf5(eps, f.total_volume());
-     while( f.round_time() < f.last_source_time() )
-      f.step();
-     while( f.round_time() < f.last_source_time() + 400.0)
-      f.step();
+     f.output_hdf5(Dielectric, f.total_volume());
+     std::vector<cdouble> field_data;
+     vec eval_pt(0.0,0.0);
+     while( f.round_time() < f.last_source_time() + 400.0 )
+      { f.step();
+        if ( f.round_time() < f.last_source_time() )
+         continue;
+        
+        cdouble HzVal = f.get_field(Hz,eval_pt);
+//printf("%e %e %e\n",f.round_time(),real(HzVal),imag(HzVal));
+        field_data.push_back( HzVal );
+      };
+     run_harminv(f, field_data, fcen, df);
 
      // (run-until (/ 1 fcen) (at-every (/ 1 fcen 20) output-hfield-z))      
-     double DeltaT = 1.0/(20*fcen);
-     double NextOutputTime = f.round_time() + DeltaT;
-     while( f.round_time() < 1.0/fcen )
+     double output_interval  = 1.0/(20*fcen);
+     double next_output_time = f.round_time() + output_interval;
+     double stop_time        = f.round_time() + 1.0/fcen;
+     while( f.round_time() < stop_time )
       { f.step();
-        if ( f.round_time() >= NextOutputTime  )
+        if ( f.round_time() >= next_output_time )
          { f.output_hdf5(Hz, f.total_volume());
-           NextOutputTime += DeltaT;
+           next_output_time += output_interval;
          };
       };
 
@@ -201,6 +238,8 @@ void holey_wvg_cavity(bool compute_modes=false)
 /***************************************************************/
 int main(int argc, char *argv[])
 {
+  initialize mpi(argc, argv);
+
   bool compute_modes=false;
   for(int narg=1; narg<argc; narg++)
    { if (!strcmp(argv[narg],"--compute-modes"))
