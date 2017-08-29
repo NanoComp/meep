582630a16b49aa3a870f7330784a5ae7243b2575
Homer Reid <homer@homerreid.com>
2017-08-28 22:45:41 -0400

    slightly modify numerical values for correct results in tests/harmonics.cpp following update to gaussian_src constructor

--- tests/harmonics.cpp
+++ tests/harmonics.cpp
@@ -103,9 +103,9 @@ int main(int argc, char **argv) {
 
   double thresh = sizeof(realnum) == sizeof(float) ? 1e-4 : 1e-5;
   harmonics(freq, 0.27e-4, 1e-4, 1.0, a2, a3);
-  if (different(a2, 9.80298e-07, thresh, "2nd harmonic mismatches known val"))
+  if (different(a2, 9.80330e-07, thresh, "2nd harmonic mismatches known val"))
     return 1;
-  if (different(a3, 9.97759e-07, thresh, "3rd harmonic mismatches known val"))
+  if (different(a3, 9.97747e-07, thresh, "3rd harmonic mismatches known val"))
     return 1;
 
   harmonics(freq, 0.54e-4, 2e-4, 1.0, a2_2, a3_2);
