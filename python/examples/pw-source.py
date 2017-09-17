d95afc86dbf7833e462c8cabf7e97a31fc77cb57
Chris Hogan <c096h800@gmail.com>
2017-09-17 13:20:44 -0500

    Add pw-source.py example

---
+++ python/examples/pw-source.py
@@ -0,0 +1,61 @@
+# This example creates an approximate TM planewave in vacuum
+# propagating at a 45-degree angle, by using a couple of current sources
+# with amplitude exp(ikx) corresponding to the desired planewave.
+from __future__ import division
+
+import math
+import meep as mp
+
+
+s = 11  # the size of the computational cell, not including PML
+dpml = 1  # thickness of PML layers
+
+sxy = s + 2 * dpml  # cell size, including PML
+cell = mp.Vector3(sxy, sxy, 0)
+
+pml_layers = [mp.Pml(dpml)]
+resolution = 10
+
+
+# pw-amp is a function that returns the amplitude exp(ik(x+x0)) at a
+# given point x.  (We need the x0 because current amplitude functions
+# in Meep are defined relative to the center of the current source,
+# whereas we want a fixed origin.)  Actually, it is a function of k
+# and x0 that returns a function of x ...
+def pw_amp(k, x0):
+    def _pw_amp(x):
+        return math.exp(1j * k.dot(x + x0))
+    return _pw_amp
+
+fcen = 0.8  # pulse center frequency
+df = 0.02  # turn-on bandwidth
+kdir = mp.Vector3(1, 1)  # direction of k (length is irrelevant)
+k = kdir.unit() * (2 * mp.PI * fcen)  # k with correct length
+
+sources = [
+    mp.Source(
+        mp.ContinuousSource(fcen, fwidth=df),
+        component=mp.Ez,
+        center=mp.Vector3(-0.5 * s, 0),
+        size=mp.Vector3(0, s),
+        amp_func=pw_amp(k, mp.Vector3(-0.5 * s, 0))
+    ),
+    mp.Source(
+        mp.ContinuousSource(fcen, fwidth=df),
+        component=mp.Ez,
+        center=mp.Vector3(0, -0.5 * s),
+        size=mp.Vector3(s, 0),
+        amp_func=pw_amp(k, mp.Vector3(0, -0.5 * s))
+    )
+]
+
+sim = mp.Simulation(
+    cell_size=cell,
+    geometry=[],
+    sources=sources,
+    boundary_layers=pml_layers,
+    resolution=resolution
+)
+
+t = 400  # run time
+sim.run(mp.at_end(mp.output_efield_z), until=t)
