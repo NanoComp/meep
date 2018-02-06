86f41728cdcc5ea75aa234cdffbfc66ea906fe5e
Chris Hogan <c096h800@gmail.com>
2018-02-06 17:09:58 -0600

    Fix 2d Absorbers

--- python/tests/absorber_1d.py
+++ python/tests/absorber_1d.py
@@ -56,6 +56,25 @@ class TestAbsorber(unittest.TestCase):
         f = self.sim.get_field_point(mp.Ex, mp.Vector3())
         self.assertAlmostEqual(f.real, 4.789444250711607e-10, places=6)
 
+    def test_absorber_2d(self):
+        source = mp.Source(
+            src=mp.GaussianSource(frequency=0.1, fwidth=0.1),
+            component=mp.Hz,
+            center=mp.Vector3()
+        )
+
+        sim = mp.Simulation(
+            cell_size=mp.Vector3(20, 20, 0),
+            resolution=10,
+            sources=[source],
+            boundary_layers=[mp.Absorber(5)]
+        )
+
+        sim.run(until_after_sources=1000)
+        v = mp.Vector3(4.13, 3.75, 0)
+        p = sim.get_field_point(mp.Hz, v)
+
+        self.assertAlmostEqual(-4.058476603571745e-11, p.real)
 
 if __name__ == '__main__':
     unittest.main()
