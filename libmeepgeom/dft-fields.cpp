3d48c2dcf36b6616380bb4f53782bfc0acc98b20
Homer Reid <homer@homerreid.com>
2018-03-15 23:18:30 +0900

    updates to libmeepgeom/dft-fields.cpp test

--- libmeepgeom/dft-fields.cpp
+++ libmeepgeom/dft-fields.cpp
@@ -34,7 +34,7 @@ double dummy_eps(const vec &) { return 1.0; }
 /***************************************************************/
 /***************************************************************/
 /***************************************************************/
-void Run(bool Pulse)
+void Run(bool Pulse, double resolution)
 {
   /***************************************************************/
   /* initialize geometry                                         */
@@ -44,8 +44,6 @@ void Run(bool Pulse)
   double r=1.0;     // inner radius of ring
   double pad=4;     // padding between waveguide and edge of PML
   double dpml=2;    // thickness of PML
-
-  double resolution = 10.0;
 
   double sxy        = 2.0*(r+w+pad+dpml);  // cell size
   geometry_lattice.size.x=sxy;
@@ -74,7 +72,7 @@ void Run(bool Pulse)
   /***************************************************************/
   /***************************************************************/
   /***************************************************************/
-  double fcen = 0.15;  // ; pulse center frequency
+  double fcen = 0.118; // ; pulse center frequency
   double df   = 0.1;   // ; df
   vec x0(r+0.1,0.0);   // ; source location
   if(Pulse)
@@ -88,22 +86,107 @@ void Run(bool Pulse)
      while( f.round_time() < f.last_source_time() + 100.0 )
       f.step();
 
-     f.output_dft(dftFlux,   "dft_flux");
-     f.output_dft(dftFields, "dft_fields");
+     f.output_dft(dftFlux,   "dft-flux");
+     f.output_dft(dftFields, "dft-fields");
    }
   else
    { 
-     f.add_point_source(Ez, continuous_src_time(fcen), x0);
-     f.solve_cw();
+     f.add_point_source(Ez, continuous_src_time(fcen,df), x0);
+     f.solve_cw(1e-8, 10000, 10);
      f.output_hdf5(Ez, f.v);
      f.output_hdf5(Hx, f.v);
      f.output_hdf5(Hy, f.v);
    }
 
-  // success if we made it here
-  // return 0;
-
 }
+
+/***************************************************************/
+/* compute the L2 norm of two complex-valued HDF5 datasets,    */
+/* after (a) normalizing each data set by its maximum amplitude*/
+/* and (b) compensating for a constant overall phase factor    */
+/* between the datasets.                                       */
+/***************************************************************/
+double compare_complex_hdf5_datasets(const char *file1, const char *name1,
+                                     const char *file2, const char *name2,
+                                     int expected_rank=2)
+{
+  char dataname[100];
+
+  // read dataset 1
+  h5file f1(file1, h5file::READONLY, false);
+  int rank1, *dims1=new int[expected_rank];
+  snprintf(dataname,100,"%s.r",name1);
+  double *rdata1 = f1.read(dataname, &rank1, dims1, expected_rank);
+  snprintf(dataname,100,"%s.i",name1);
+  double *idata1 = f1.read(dataname, &rank1, dims1, expected_rank);
+  if (!rdata1 || !idata1) return -1.0;
+
+  // read dataset 2
+  h5file f2(file2, h5file::READONLY, false);
+  int rank2, *dims2=new int[expected_rank];
+  snprintf(dataname,100,"%s.r",name2);
+  double *rdata2 = f2.read(dataname, &rank2, dims2, expected_rank);
+  snprintf(dataname,100,"%s.i",name2);
+  double *idata2 = f2.read(dataname, &rank2, dims2, expected_rank);
+  if (!rdata2 || !idata2) return -1.0;
+
+  // check same size
+  bool same_size = (rank1==rank2);
+  for(int d=0; same_size && d<rank1; d++)
+   if(dims1[d]!=dims2[d])
+    same_size = false;
+  if (!same_size)
+   meep::abort("data size mismatch\n");
+
+  // first pass to normalize each dataset to its maximum absolute magnitude;
+  // we also note the phase difference between the datasets at their points
+  // of maximum magnitude so we can compensate for this in the comparison below.
+  int length = dims1[0];
+  for(int d=1; d<rank1; d++)
+   length*=dims1[d];
+
+  double max_abs1=0.0, max_abs2=0.0;
+  double max_arg1=0.0, max_arg2=0.0;
+  for(int n=0; n<length; n++)
+   { cdouble z1 = cdouble(rdata1[n], idata1[n]);
+     if (abs(z1) > max_abs1)
+      { max_abs1 = abs(z1);
+        max_arg1 = arg(z1);
+      }
+     cdouble z2 = cdouble(rdata2[n], idata2[n]);
+     if (abs(z2) > max_abs2)
+      { max_abs2 = abs(z2);
+        max_arg2 = arg(z2);
+      }
+   }
+
+  // second pass to get L2 norm of difference between normalized data sets
+  double norm1=0.0, norm2=0.0, normdiff=0.0;
+  cdouble phase_fac = exp( cdouble(0,1)*(max_arg1 - max_arg2) );
+  for(int n=0; n<length; n++)
+   { cdouble z1 = cdouble(rdata1[n], idata1[n]) / max_abs1;
+     cdouble z2 = phase_fac*cdouble(rdata2[n], idata2[n]) / max_abs2;
+     norm1    += norm(z1);
+     norm2    += norm(z2);
+     normdiff += norm(z1-z2);
+   }
+  norm1    = sqrt(norm1)    / ((double)length);
+  norm2    = sqrt(norm2)    / ((double)length);
+  normdiff = sqrt(normdiff) / ((double)length);
+
+  if (am_master())
+   { FILE *ff=fopen("/tmp/dft-fields.log","a");
+     fprintf(ff,"length %i:\n",length);
+     fprintf(ff,"norm1    : %e\n",norm1);
+     fprintf(ff,"norm2    : %e\n",norm2);
+     fprintf(ff,"normdiff : %e\n",normdiff);
+     fprintf(ff,"\n");
+     fclose(ff);
+   }
+
+  return normdiff / fmax(norm1,norm2);
+}
+
 
 /***************************************************************/
 /***************************************************************/
@@ -111,6 +194,29 @@ void Run(bool Pulse)
 int main(int argc, char *argv[])
 {
   initialize mpi(argc, argv);
-  Run(true);
-  Run(false);
+
+#define NUM_RESOLUTIONS 3    
+  double resolutions[NUM_RESOLUTIONS]={10.0, 20.0, 40.0};
+  double L2Errors[NUM_RESOLUTIONS];
+  for(int nr=0; nr<NUM_RESOLUTIONS; nr++)
+   {
+     double res = resolutions[nr];
+     Run(true,  res);
+     Run(false, res);
+
+     L2Errors[nr] 
+      = compare_complex_hdf5_datasets("dft-fields.h5","ez_0",
+                                      "ez-000000.05.h5","ez",2);
+     if (L2Errors[nr]==-1.0) // files couldn't be read or datasets had different sizes
+      return 1;
+   }
+
+  // condition for test to pass is simply that L2 norm decreases
+  // with increasing resolution; note that this is also implicitly
+  // testing the success of the dft_fields and output_dft 
+  // implementations
+  if ( (L2Errors[0] > L2Errors[1]) && (L2Errors[1] > L2Errors[2]) )
+   return 0;
+
+  return 1;
 }
