57d084972efe1b3bf044d159644f8a221eed9a1d
Homer Reid <homer@homerreid.com>
2018-04-18 20:28:44 -0400

    updates

--- src/mpb.cpp
+++ src/mpb.cpp
@@ -655,8 +655,8 @@ void fields::get_eigenmode_coefficients(dft_flux flux,
       cdouble cminus  = 0.5*(mode_flux[0] - mode_flux[1]);
       cdouble normfac = 0.5*(mode_mode[0] + mode_mode[1]); // = vgrp * flux_volume(flux)
       if (normfac==0.0) normfac=1.0;
-      coeffs[ 2*nb*num_freqs + 2*nf + (vg>0.0 ? 0 : 1) ] = cplus/sqrt(normfac);
-      coeffs[ 2*nb*num_freqs + 2*nf + (vg>0.0 ? 1 : 0) ] = cminus/sqrt(normfac);
+      coeffs[ 2*nb*num_freqs + 2*nf + (vg>0.0 ? 0 : 1) ] = cplus/sqrt(abs(normfac));
+      coeffs[ 2*nb*num_freqs + 2*nf + (vg>0.0 ? 1 : 0) ] = cminus/sqrt(abs(normfac));
     }
 }
 
