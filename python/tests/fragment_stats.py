4ec3b0e64cb4052d151701ef9b99a75f1869ea9b
Chris Hogan <c096h800@gmail.com>
2018-05-24 13:50:12 -0500

    More DFT testing

--- python/tests/fragment_stats.py
+++ python/tests/fragment_stats.py
@@ -2,14 +2,15 @@ import unittest
 import meep as mp
 
 
-def make_dft_vecs(flxc, flxs, n2fc, n2fs, frcc, frcs):
+def make_dft_vecs(flx_reg=None, n2f_reg=None, frc_reg=None, fldc=None, flds=None, fldw=None, fld_cmp=None):
     dft_vecs = {
-        'flux_center': flxc,
-        'flux_size': flxs,
-        'n2f_center': n2fc,
-        'n2f_size': n2fs,
-        'force_center': frcc,
-        'force_size': frcs,
+        'flux_regions': flx_reg,
+        'n2f_regions': n2f_reg,
+        'force_regions': frc_reg,
+        'fields_center': fldc,
+        'fields_size': flds,
+        'fields_where': fldw,
+        'fields_components': fld_cmp
     }
     return dft_vecs
 
@@ -42,12 +43,15 @@ class TestFragmentStats(unittest.TestCase):
                             default_material=def_mat)
 
         if dft_vecs:
-            fluxr = mp.FluxRegion(dft_vecs['flux_center'], size=dft_vecs['flux_size'])
-            sim.add_flux(1, 0.5, 5, fluxr)
-            n2fr = mp.Near2FarRegion(dft_vecs['n2f_center'], size=dft_vecs['n2f_size'])
-            sim.add_near2far(1, 0.5, 7, n2fr)
-            forcer = mp.ForceRegion(dft_vecs['force_center'], mp.X, size=dft_vecs['force_size'])
-            sim.add_force(1, 0.5, 9, forcer)
+            if dft_vecs['flux_regions']:
+                sim.add_flux(1, 0.5, 5, *dft_vecs['flux_regions'])
+            if dft_vecs['n2f_regions']:
+                sim.add_near2far(1, 0.5, 7, *dft_vecs['n2f_regions'])
+            if dft_vecs['force_regions']:
+                sim.add_force(1, 0.5, 9, *dft_vecs['force_regions'])
+            if dft_vecs['fields_components']:
+                sim.add_dft_fields(dft_vecs['fields_components'], 0, 1, 5, where=dft_vecs['fields_where'],
+                                   center=dft_vecs['fields_center'], size=dft_vecs['fields_size'])
 
         gv = sim._create_grid_volume(False)
         stats = sim._compute_fragment_stats(gv)
@@ -60,12 +64,9 @@ class TestFragmentStats(unittest.TestCase):
 
         # flux covering first fragment, near2far covering second, force covering third
         dft_vecs = make_dft_vecs(
-            mp.Vector3(z=-10),
-            mp.Vector3(z=10),
-            mp.Vector3(),
-            mp.Vector3(z=10),
-            mp.Vector3(z=10),
-            mp.Vector3(z=10)
+            [mp.FluxRegion(mp.Vector3(z=-10), size=mp.Vector3(z=10))],
+            [mp.Near2FarRegion(mp.Vector3(), size=mp.Vector3(z=10))],
+            [mp.ForceRegion(mp.Vector3(z=10), mp.X, size=mp.Vector3(z=10))]
         )
 
         fs = self.get_fragment_stats(mp.Vector3(z=10), mp.Vector3(z=30), 1, dft_vecs=dft_vecs)
@@ -104,7 +105,14 @@ class TestFragmentStats(unittest.TestCase):
         # A cell with z=26, with a 16 unit block in the center, split into 3 fragments,
         # with the first and last fragment of length 8, and 3/8 covered by the block,
         # and the middle fragment completely covered.
-        fs = self.get_fragment_stats(mp.Vector3(z=16), mp.Vector3(z=26), 1)
+
+        # dft_flux with 2 volumes, 1 covering the first fragment and one covering
+        # half of the second fragment
+        dft_vecs = make_dft_vecs(flx_reg=[
+            mp.FluxRegion(mp.Vector3(z=-9), mp.Vector3(z=8)),
+            mp.FluxRegion(mp.Vector3(z=-2.5), mp.Vector3(z=5))
+        ])
+        fs = self.get_fragment_stats(mp.Vector3(z=16), mp.Vector3(z=26), 1, dft_vecs=dft_vecs)
 
         self.assertEqual(len(fs), 3)
         # Check first and last box sizes
@@ -113,11 +121,16 @@ class TestFragmentStats(unittest.TestCase):
         self.assertEqual(fs[2].box.low.z, 5)
         self.assertEqual(fs[2].box.high.z, 13)
 
-        # Middel fragment is completely covered by block
+        # Middle fragment is completely covered by block
         self.check_stats(fs[1], a_eps=100, a_mu=100, nonlin=300, susc=300, cond=300)
         # Outer fragments are 3/8 covered
         for i in [0, 2]:
             self.check_stats(fs[i], a_eps=30, a_mu=30, nonlin=90, susc=90, cond=90)
+
+        # Check dft stats
+        self.assertEqual(fs[0].num_dft_pixels, 1600)
+        self.assertEqual(fs[1].num_dft_pixels, 1000)
+        self.assertEqual(fs[2].num_dft_pixels, 0)
 
     def test_1d_with_shifted_center(self):
         # A cell with z=26, with a 16 unit block shifted so that the right side is flush,
@@ -142,17 +155,38 @@ class TestFragmentStats(unittest.TestCase):
         # Last fragment is completely covered, but only 8 units long
         self.check_stats(fs[2], a_eps=80, a_mu=80, nonlin=240, susc=240, cond=240)
 
+    def test_1d_dft_fields(self):
+        # A z=30 cell, split into three fragments of size 10 each, with a block
+        # covering the middle fragment.
+
+        # dft_fields covering first fragment
+        dft_vecs = make_dft_vecs(fldc=mp.Vector3(z=-10), flds=mp.Vector3(z=10), fld_cmp=[mp.X, mp.Y])
+        fs = self.get_fragment_stats(mp.Vector3(z=10), mp.Vector3(z=30), 1, dft_vecs=dft_vecs)
+
+        self.assertEqual(len(fs), 3)
+
+        for i in [1, 2]:
+            self.assertEqual(fs[i].num_dft_pixels, 0)
+
+        self.assertEqual(fs[0].num_dft_pixels, 1000)
+
+        # Same test with volume instead of center and size
+        dft_vecs = make_dft_vecs(fldw=mp.Volume(mp.Vector3(z=-10), mp.Vector3(z=10)), fld_cmp=[mp.X, mp.Y])
+        fs = self.get_fragment_stats(mp.Vector3(z=10), mp.Vector3(z=30), 1, dft_vecs=dft_vecs)
+
+        for i in [1, 2]:
+            self.assertEqual(fs[i].num_dft_pixels, 0)
+
+        self.assertEqual(fs[0].num_dft_pixels, 1000)
+
     def test_2d(self):
         # A 30 x 30 cell, with a 10 x 10 block in the middle, split into 9 10 x 10 fragments.
 
         # flux covering top-left fragment, near2far covering top-middle, force covering top-right
         dft_vecs = make_dft_vecs(
-            mp.Vector3(-10, 10),
-            mp.Vector3(10, 10),
-            mp.Vector3(0, 10),
-            mp.Vector3(10, 10),
-            mp.Vector3(10, 10),
-            mp.Vector3(10, 10)
+            [mp.FluxRegion(mp.Vector3(-10, 10), size=mp.Vector3(10, 10))],
+            [mp.Near2FarRegion(mp.Vector3(0, 10), size=mp.Vector3(10, 10))],
+            [mp.ForceRegion(mp.Vector3(10, 10), mp.X, size=mp.Vector3(10, 10))]
         )
         fs = self.get_fragment_stats(mp.Vector3(10, 10), mp.Vector3(30, 30), 2, dft_vecs=dft_vecs)
 
@@ -247,6 +281,28 @@ class TestFragmentStats(unittest.TestCase):
         self.assertEqual(fs[6].box.high.y, -5)
         self.check_stats(fs[6], a_eps=640, a_mu=640, nonlin=1920, susc=1920, cond=1920)
 
+    def test_2d_dft_fields(self):
+        # A 30 x 30 cell, with a 10 x 10 block in the middle, split into 9 10 x 10 fragments.
+
+        # dft_fields covering 20 by 20 area in center of cell. Test with volume, and center/size
+        cmpts = [mp.Ex, mp.Ey, mp.Ez]
+        dft_fields_size_center = make_dft_vecs(fldc=mp.Vector3(), flds=mp.Vector3(20, 20), fld_cmp=cmpts)
+        dft_fields_where = make_dft_vecs(fldw=mp.Volume(mp.Vector3(), mp.Vector3(20, 20)), fld_cmp=cmpts)
+
+        for dft_vec in [dft_fields_size_center, dft_fields_where]:
+            fs = self.get_fragment_stats(mp.Vector3(10, 10), mp.Vector3(30, 30), 2, dft_vecs=dft_vec)
+
+            # Middle fragment is fully covered
+            self.assertEqual(fs[4].num_dft_pixels, 15000)
+
+            # 4 corners are 1/4 covered
+            for i in [0, 2, 6, 8]:
+                self.assertEqual(fs[i].num_dft_pixels, 3750)
+
+            # The rest are half covered
+            for i in [1, 3, 5, 7]:
+                self.assertEqual(fs[i].num_dft_pixels, 7500)
+
     def test_3d(self):
         # A 30 x 30 x 30 cell with a 10 x 10 x 10 block placed at the center, split
         # into 27 10 x 10 x 10 fragments
@@ -254,12 +310,9 @@ class TestFragmentStats(unittest.TestCase):
         # flux covering lower-front-left fragment, near2far covering lower-middle-left,
         # force covering lower-back-left
         dft_vecs = make_dft_vecs(
-            mp.Vector3(-10, -10, -10),
-            mp.Vector3(10, 10, 10),
-            mp.Vector3(-10, -10, 0),
-            mp.Vector3(10, 10, 10),
-            mp.Vector3(-10, -10, 10),
-            mp.Vector3(10, 10, 10)
+            [mp.FluxRegion(mp.Vector3(-10, -10, -10), size=mp.Vector3(10, 10, 10))],
+            [mp.Near2FarRegion(mp.Vector3(-10, -10, 0), size=mp.Vector3(10, 10, 10))],
+            [mp.ForceRegion(mp.Vector3(-10, -10, 10), mp.X, size=mp.Vector3(10, 10, 10))]
         )
         fs = self.get_fragment_stats(mp.Vector3(10, 10, 10), mp.Vector3(30, 30, 30), 3, dft_vecs=dft_vecs)
 
@@ -314,12 +367,9 @@ class TestFragmentStats(unittest.TestCase):
 
         # flux covering top-left fragment, near2far covering top-middle, force covering top-right
         dft_vecs = make_dft_vecs(
-            mp.Vector3(-10, z=10),
-            mp.Vector3(10, z=10),
-            mp.Vector3(0, z=10),
-            mp.Vector3(10, z=10),
-            mp.Vector3(10, z=10),
-            mp.Vector3(10, z=10)
+            [mp.FluxRegion(mp.Vector3(-10, z=10), size=mp.Vector3(10, z=10))],
+            [mp.Near2FarRegion(mp.Vector3(0, z=10), size=mp.Vector3(10, z=10))],
+            [mp.ForceRegion(mp.Vector3(10, z=10), mp.X, size=mp.Vector3(10, z=10))]
         )
         fs = self.get_fragment_stats(mp.Vector3(10, 0, 10), mp.Vector3(30, 0, 30),
                                      mp.CYLINDRICAL, dft_vecs=dft_vecs)
