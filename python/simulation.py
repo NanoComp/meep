fb6071172b1c6bd94b8fc599b7e4773fe6ee0637
Chris Hogan <c096h800@gmail.com>
2018-08-14 11:03:54 -0500

    Fix bugs in _get_dft_data and _load_dft_data

--- python/simulation.py
+++ python/simulation.py
@@ -1030,9 +1030,11 @@ class Simulation(object):
 
     def get_dft_data(self, dft_chunk):
         n = mp._get_dft_data_size(dft_chunk)
-        arr = np.zeros(n, np.complex128)
-        mp._get_dft_data(dft_chunk, arr)
-        return arr
+        my_arr = np.zeros(n, np.complex128)
+        result = np.zeros(n, np.complex128)
+        mp._get_dft_data(dft_chunk, my_arr)
+        mp.sum_to_all(my_arr, result, n)
+        return result
 
     def add_near2far(self, fcen, df, nfreq, *near2fars):
         n2f = DftNear2Far(self._add_near2far, [fcen, df, nfreq, near2fars])
