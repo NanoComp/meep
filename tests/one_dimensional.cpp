a54d50534f79f241fb8f237a942e7eb80045c5da
Ardavan Oskooi <ardavan.oskooi@gmail.com>
2019-01-03 20:38:53 -0800

    remove initialize_field test tests

--- tests/one_dimensional.cpp
+++ tests/one_dimensional.cpp
@@ -107,59 +107,11 @@ int test_simple_periodic(double eps(const vec &), int splitting, const char *myd
   return 1;
 }
 
-complex<double> checkers(const vec &pt) {
-  const double thez = pt.z()+0.00001;
-  int z = (int) (thez*5.0);
-  int zz = (int) (thez*10.0);
-  if (z & 1) return cos(thez);
-  if (zz & 1) return 2.0;
-  return 1.0;
-}
-
-int test_pattern(double eps(const vec &), int splitting,
-                 const char *mydirname) {
-  double a = 10.0;
-  grid_volume gv = volone(6.0,a);
-  structure s1(gv, eps);
-  structure s(gv, eps, no_pml(), identity(), splitting);
-  s.set_output_directory(mydirname);
-  s1.set_output_directory(mydirname);
-
-  master_printf("Trying test pattern with %d chunks...\n", splitting);
-  fields f(&s);
-  f.use_bloch(0.0);
-  fields f1(&s1);
-  f1.use_bloch(0.0);
-  if (!compare(f1.count_volume(Ex), f.count_volume(Ex), "grid_volume")) return 0;
-  f1.initialize_field(Hy, checkers);
-  f.initialize_field(Hy, checkers);
-
-  f.step();
-  f1.step();
-  if (!compare_point(f, f1, vec(27.99))) return 0;
-  if (!compare_point(f, f1, vec(42.01))) return 0;
-  if (!compare_point(f, f1, vec(0.751))) return 0;
-  if (!compare_point(f, f1, vec(0.01 ))) return 0;
-  if (!compare_point(f, f1, vec(1.0  ))) return 0;
-  if (!compare(f.field_energy(), f1.field_energy(),
-               "   total energy")) return 0;
-  if (!compare(f.electric_energy_in_box(gv.surroundings()),
-               f1.electric_energy_in_box(gv.surroundings()),
-               "electric energy")) return 0;
-  if (!compare(f.magnetic_energy_in_box(gv.surroundings()),
-               f1.magnetic_energy_in_box(gv.surroundings()),
-               "magnetic energy")) return 0;
-  return 1;
-}
-
 int main(int argc, char **argv) {
   initialize mpi(argc, argv);
   quiet = true;
   const char *mydirname = "one_dimensional-out";
   master_printf("Testing one dimension under different splittings...\n");
-
-  for (int s=2;s<7;s++)
-    if (!test_pattern(one, s, mydirname)) abort("error in test_pattern\n");
 
   for (int s=2;s<7;s++)
     if (!test_simple_periodic(one, s, mydirname)) abort("error in test_simple_periodic\n");
