5a749957a674b55b6aa94ea4b23a07b8361776ee
Homer Reid <homereid2@gmail.com>
2019-04-20 00:00:27 +0200

    GDSII layers (#817)
    * updates
    
    * add configure check for libGDSII::GetLayers
    
    * better error message

--- src/GDSIIgeom.cpp
+++ src/GDSIIgeom.cpp
@@ -292,4 +292,15 @@ meep::volume get_GDSII_volume(const char *GDSIIFile, int Layer, double zmin, dou
 
 #endif // HAVE_LIBGDSII
 
+std::vector<int> get_GDSII_layers(const char *GDSIIFile)
+{
+#if defined(HAVE_LIBGDSII) && defined(HAVE_GDSII_GETLAYERS)
+  return libGDSII::GetLayers(GDSIIFile);
+#else
+  GDSIIError("get_GDSII_layers (needs libGDSII version 0.21 or later)");
+  std::vector<int> layers;
+  return layers;
+#endif
+}
+
 } // namespace meep_geom
