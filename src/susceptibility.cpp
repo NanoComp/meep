14454cf9fdf2c4d6a7016e094526b7cc668e2098
Chong Yidong <cyd@gnu.org>
2019-05-24 11:51:24 +0800

    Fix last change to update_P

--- src/susceptibility.cpp
+++ src/susceptibility.cpp
@@ -349,8 +349,8 @@ void gyrotropic_susceptibility::update_P(realnum *W[NUM_FIELD_COMPONENTS][2],
   FOR_COMPONENTS(c) DOCMP2 {
     if (d->P[c][cmp]) {
       const direction d0 = component_direction(c);
-      const realnum *w = W[c][cmp], *s = 2*pi*sigma[c][d0];
-      if (w && s) {
+      const realnum *w = W[c][cmp];
+      if (w) {
 	if (d0 != X && d0 != Y && d0 != Z)
 	  abort("Cylindrical coordinates are not supported for gyrotropic media");
 
@@ -366,8 +366,8 @@ void gyrotropic_susceptibility::update_P(realnum *W[NUM_FIELD_COMPONENTS][2],
         const realnum *w1 = W[c1][cmp];
         const realnum *w2 = W[c2][cmp];
 
-        const realnum *s1 = w1 ? 2*pi*sigma[c1][d1] : NULL;
-        const realnum *s2 = w2 ? 2*pi*sigma[c2][d2] : NULL;
+        const realnum *s1 = w1 ? sigma[c1][d1] : NULL;
+        const realnum *s2 = w2 ? sigma[c2][d2] : NULL;
 
         const realnum *p1 = d->P[c1][cmp];
         const realnum *p2 = d->P[c2][cmp];
@@ -375,8 +375,8 @@ void gyrotropic_susceptibility::update_P(realnum *W[NUM_FIELD_COMPONENTS][2],
 	const realnum vab1 = va * gyro_tensor[d0][d1];
 	const realnum vab2 = va * gyro_tensor[d0][d2];
 
-	const realnum ndt1 = dt * gyro_tensor[d0][d1];
-	const realnum ndt2 = dt * gyro_tensor[d0][d2];
+	const realnum ndt1 = 2*pi*dt * gyro_tensor[d0][d1];
+	const realnum ndt2 = 2*pi*dt * gyro_tensor[d0][d2];
 
 	LOOP_OVER_VOL_OWNED(gv, c, i) {
 	  pp[i] = ua * p[i];
