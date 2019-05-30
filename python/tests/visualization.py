0fb7f51ce3e4bad6013bf1132259c947cfc1712b
smartalecH <alec.m.hammond@gmail.com>
2019-05-30 10:32:27 -0600

    add test file
    update travis
    
    add pyqt to travis
    
    revise qt in travis
    
    fixes for python2
    
    switch from qt to wxpython
    
    try again
    
    try pyside
    
    again
    
    and again
    
    better error catching and remove mayavi from travis and test for now
    
    remove modified jupyter notebooks

---
+++ python/tests/visualization.py
@@ -0,0 +1,125 @@
+
+# visualization.py - Tests the visualization module. Checks 2D 
+# plotting of a waveguide with several sources, monitors, and
+# boundary conditions. Checks for subdomain plots.
+#
+# Also tests the animation run function, mp4 output, jshtml output, and git output.
+
+from __future__ import division
+
+import unittest
+import meep as mp
+import numpy as np
+
+# Make sure we have matplotlib installed
+import matplotlib
+matplotlib.use('agg') # Set backend for consistency and to pull pixels quickly
+from matplotlib import pyplot as plt
+
+# Make sure we have mayavi installed
+import mayavi
+
+def hash_figure(fig):
+    fig.canvas.draw()
+    data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,)).flatten()
+    hash = np.sum((data > np.mean(data)) + data)
+    return hash
+
+def setup_sim(zDim=0):
+    cell = mp.Vector3(16,8,zDim)
+
+    # A simple waveguide
+    geometry = [mp.Block(mp.Vector3(mp.inf,1,1),
+                     center=mp.Vector3(),
+                     material=mp.Medium(epsilon=12))]
+    
+    # A point source
+    sources = [mp.Source(mp.ContinuousSource(frequency=0.15),
+                     component=mp.Ez,
+                     center=mp.Vector3(-5,0))]
+    
+    # A line source
+    sources += [mp.Source(mp.ContinuousSource(frequency=0.15),
+                     component=mp.Ez,
+                     size=mp.Vector3(0,2,0),
+                     center=mp.Vector3(-6,0))]
+        
+    # A plane source
+    sources += [mp.Source(mp.ContinuousSource(frequency=0.15),
+                     component=mp.Ez,
+                     size=mp.Vector3(2,2,0),
+                     center=mp.Vector3(-3,0))]
+    
+    # Different pml layers
+    pml_layers = [mp.PML(2.0,mp.X),mp.PML(1.0,mp.Y,mp.Low),mp.PML(1.5,mp.Y,mp.High)]
+
+    resolution = 10
+
+    sim = mp.Simulation(cell_size=cell,
+                    boundary_layers=pml_layers,
+                    geometry=geometry,
+                    sources=sources,
+                    resolution=resolution)
+    # Line monitor
+    sim.add_flux(1,0,1,mp.FluxRegion(center=mp.Vector3(5,0,0),size=mp.Vector3(0,4), direction=mp.X))
+
+    # Plane monitor
+    sim.add_flux(1,0,1,mp.FluxRegion(center=mp.Vector3(2,0,0),size=mp.Vector3(4,4), direction=mp.X))
+    
+    return sim
+
+class TestVisualization(unittest.TestCase):
+    
+    def test_plot2D(self):
+        
+        # Check plotting of geometry with several sources, monitors, and PMLs
+        f = plt.figure()
+        ax = f.gca()
+        sim = setup_sim()
+        ax = sim.plot2D(ax=ax)
+        self.assertAlmostEqual(hash_figure(f),10231693)
+
+        # Check plotting of fields after timestepping
+        f = plt.figure()
+        ax = f.gca()
+        sim.run(until=200)
+        ax = sim.plot2D(ax=ax,fields=mp.Ez)
+        self.assertAlmostEqual(hash_figure(f),79810954)
+
+        # Check output_plane feature
+        f = plt.figure()
+        ax = f.gca()
+        vol = mp.Volume(center=mp.Vector3(),size=mp.Vector3(2,2))
+        ax = sim.plot2D(ax=ax,fields=mp.Ez,output_plane=vol)
+        self.assertAlmostEqual(hash_figure(f),68944594)
+    
+    def test_animation_output(self):
+        # Check without normalization
+        f = plt.figure()
+        sim = setup_sim()
+        animation = mp.Animate2D(sim,mp.Ez,f,normalize=False)
+        sim.run(mp.at_every(1),until=25)
+
+        # Check with normalization
+        animation = mp.Animate2D(sim,mp.Ez,f,normalize=True)
+        sim.run(mp.at_every(1),until=25)
+
+        # Check mp4 output
+        animation.to_mp4(10,'test.mp4')
+
+        # Check gif output
+        animation.to_gif(10,'test.gif')
+
+        # Check jshtml output
+        animation.to_jshtml(10)
+    '''
+    Travis does not play well with Mayavi
+    def test_3D_mayavi(self):
+        sim = setup_sim(4)
+        sim.plot3D()
+    '''
+
+if __name__ == '__main__':
+    unittest.main()
+    
--- python/visualization.py
+++ python/visualization.py
@@ -321,6 +321,8 @@ def plot_eps(sim,ax,output_plane=None,eps_parameters=None):
         extent = [xmin,xmax,ymin,ymax]
         xlabel = 'X'
         ylabel = 'Y'
+    else:
+        raise ValueError("A 2D plane has not been specified...")
 
     eps_data = np.rot90(np.real(sim.get_array(center=center, size=cell_size, component=mp.Dielectric)))
     ax.imshow(eps_data, extent=extent, **eps_parameters)
@@ -495,8 +497,12 @@ def plot2D(sim,ax=None, output_plane=None, fields=None, labels=False,
         from matplotlib import pyplot as plt
         ax = plt.gca()
 
+    # User incorrectly specified a 3D output plane
     if output_plane and (output_plane.size.x != 0) and (output_plane.size.y != 0) and (output_plane.size.z != 0):
-        raise ValueError("output_plane must be a 2 dimensional volume (a plane)")
+        raise ValueError("output_plane must be a 2 dimensional volume (a plane).")
+    # User forgot to specify a 2D output plane for a 3D simulation
+    elif output_plane is None and (sim.cell_size.x != 0) and (sim.cell_size.y != 0) and (sim.cell_size.z != 0):
+        raise ValueError("For 3D simulations, you must specify an output_plane.")
     
     # Plot geometry
     ax = plot_eps(sim,ax,output_plane=output_plane,eps_parameters=eps_parameters)
@@ -714,9 +720,6 @@ class Animate2D(object):
         # Exports a javascript enabled html object that is
         # ready for jupyter notebook embedding.
         # modified from matplotlib/animation.py code.
-
-        from matplotlib._animation_data import (
-        DISPLAY_TEMPLATE, INCLUDED_FRAMES, JS_INCLUDE, STYLE_INCLUDE)
         from uuid import uuid4
 
         # save the frames to an html file
@@ -819,3 +822,255 @@ class JS_Animation():
         return self.jshtml
     def get_jshtml(self):
         return self.jshtml
+
+
+# ------------------------------------------------------- #
+# JSHTML Templates
+# ------------------------------------------------------- #
+# Template functions grabbed from matplotlib/_animation_data.py
+# https://github.com/matplotlib/matplotlib/blob/aac57266803ff09c8c9dc10e1bb2bd389d79e42d/lib/matplotlib/_animation_data.py
+# Included here since python2 only supports matplotlib <3.0
+
+
+# Javascript template for HTMLWriter
+JS_INCLUDE = """
+<link rel="stylesheet"
+href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/
+css/font-awesome.min.css">
+<script language="javascript">
+  function isInternetExplorer() {
+    ua = navigator.userAgent;
+    /* MSIE used to detect old browsers and Trident used to newer ones*/
+    return ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+  }
+  /* Define the Animation class */
+  function Animation(frames, img_id, slider_id, interval, loop_select_id){
+    this.img_id = img_id;
+    this.slider_id = slider_id;
+    this.loop_select_id = loop_select_id;
+    this.interval = interval;
+    this.current_frame = 0;
+    this.direction = 0;
+    this.timer = null;
+    this.frames = new Array(frames.length);
+    for (var i=0; i<frames.length; i++)
+    {
+     this.frames[i] = new Image();
+     this.frames[i].src = frames[i];
+    }
+    var slider = document.getElementById(this.slider_id);
+    slider.max = this.frames.length - 1;
+    if (isInternetExplorer()) {
+        // switch from oninput to onchange because IE <= 11 does not conform
+        // with W3C specification. It ignores oninput and onchange behaves
+        // like oninput. In contrast, Mircosoft Edge behaves correctly.
+        slider.setAttribute('onchange', slider.getAttribute('oninput'));
+        slider.setAttribute('oninput', null);
+    }
+    this.set_frame(this.current_frame);
+  }
+  Animation.prototype.get_loop_state = function(){
+    var button_group = document[this.loop_select_id].state;
+    for (var i = 0; i < button_group.length; i++) {
+        var button = button_group[i];
+        if (button.checked) {
+            return button.value;
+        }
+    }
+    return undefined;
+  }
+  Animation.prototype.set_frame = function(frame){
+    this.current_frame = frame;
+    document.getElementById(this.img_id).src =
+            this.frames[this.current_frame].src;
+    document.getElementById(this.slider_id).value = this.current_frame;
+  }
+  Animation.prototype.next_frame = function()
+  {
+    this.set_frame(Math.min(this.frames.length - 1, this.current_frame + 1));
+  }
+  Animation.prototype.previous_frame = function()
+  {
+    this.set_frame(Math.max(0, this.current_frame - 1));
+  }
+  Animation.prototype.first_frame = function()
+  {
+    this.set_frame(0);
+  }
+  Animation.prototype.last_frame = function()
+  {
+    this.set_frame(this.frames.length - 1);
+  }
+  Animation.prototype.slower = function()
+  {
+    this.interval /= 0.7;
+    if(this.direction > 0){this.play_animation();}
+    else if(this.direction < 0){this.reverse_animation();}
+  }
+  Animation.prototype.faster = function()
+  {
+    this.interval *= 0.7;
+    if(this.direction > 0){this.play_animation();}
+    else if(this.direction < 0){this.reverse_animation();}
+  }
+  Animation.prototype.anim_step_forward = function()
+  {
+    this.current_frame += 1;
+    if(this.current_frame < this.frames.length){
+      this.set_frame(this.current_frame);
+    }else{
+      var loop_state = this.get_loop_state();
+      if(loop_state == "loop"){
+        this.first_frame();
+      }else if(loop_state == "reflect"){
+        this.last_frame();
+        this.reverse_animation();
+      }else{
+        this.pause_animation();
+        this.last_frame();
+      }
+    }
+  }
+  Animation.prototype.anim_step_reverse = function()
+  {
+    this.current_frame -= 1;
+    if(this.current_frame >= 0){
+      this.set_frame(this.current_frame);
+    }else{
+      var loop_state = this.get_loop_state();
+      if(loop_state == "loop"){
+        this.last_frame();
+      }else if(loop_state == "reflect"){
+        this.first_frame();
+        this.play_animation();
+      }else{
+        this.pause_animation();
+        this.first_frame();
+      }
+    }
+  }
+  Animation.prototype.pause_animation = function()
+  {
+    this.direction = 0;
+    if (this.timer){
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+  }
+  Animation.prototype.play_animation = function()
+  {
+    this.pause_animation();
+    this.direction = 1;
+    var t = this;
+    if (!this.timer) this.timer = setInterval(function() {
+        t.anim_step_forward();
+    }, this.interval);
+  }
+  Animation.prototype.reverse_animation = function()
+  {
+    this.pause_animation();
+    this.direction = -1;
+    var t = this;
+    if (!this.timer) this.timer = setInterval(function() {
+        t.anim_step_reverse();
+    }, this.interval);
+  }
+</script>
+"""
+
+
+# Style definitions for the HTML template
+STYLE_INCLUDE = """
+<style>
+.animation {
+    display: inline-block;
+    text-align: center;
+}
+input[type=range].anim-slider {
+    width: 374px;
+    margin-left: auto;
+    margin-right: auto;
+}
+.anim-buttons {
+    margin: 8px 0px;
+}
+.anim-buttons button {
+    padding: 0;
+    width: 36px;
+}
+.anim-state label {
+    margin-right: 8px;
+}
+.anim-state input {
+    margin: 0;
+    vertical-align: middle;
+}
+</style>
+"""
+
+
+# HTML template for HTMLWriter
+DISPLAY_TEMPLATE = """
+<div class="animation">
+  <img id="_anim_img{id}">
+  <div class="anim-controls">
+    <input id="_anim_slider{id}" type="range" class="anim-slider"
+           name="points" min="0" max="1" step="1" value="0"
+           oninput="anim{id}.set_frame(parseInt(this.value));"></input>
+    <div class="anim-buttons">
+      <button onclick="anim{id}.slower()"><i class="fa fa-minus"></i></button>
+      <button onclick="anim{id}.first_frame()"><i class="fa fa-fast-backward">
+          </i></button>
+      <button onclick="anim{id}.previous_frame()">
+          <i class="fa fa-step-backward"></i></button>
+      <button onclick="anim{id}.reverse_animation()">
+          <i class="fa fa-play fa-flip-horizontal"></i></button>
+      <button onclick="anim{id}.pause_animation()"><i class="fa fa-pause">
+          </i></button>
+      <button onclick="anim{id}.play_animation()"><i class="fa fa-play"></i>
+          </button>
+      <button onclick="anim{id}.next_frame()"><i class="fa fa-step-forward">
+          </i></button>
+      <button onclick="anim{id}.last_frame()"><i class="fa fa-fast-forward">
+          </i></button>
+      <button onclick="anim{id}.faster()"><i class="fa fa-plus"></i></button>
+    </div>
+    <form action="#n" name="_anim_loop_select{id}" class="anim-state">
+      <input type="radio" name="state" value="once" id="_anim_radio1_{id}"
+             {once_checked}>
+      <label for="_anim_radio1_{id}">Once</label>
+      <input type="radio" name="state" value="loop" id="_anim_radio2_{id}"
+             {loop_checked}>
+      <label for="_anim_radio2_{id}">Loop</label>
+      <input type="radio" name="state" value="reflect" id="_anim_radio3_{id}"
+             {reflect_checked}>
+      <label for="_anim_radio3_{id}">Reflect</label>
+    </form>
+  </div>
+</div>
+<script language="javascript">
+  /* Instantiate the Animation class. */
+  /* The IDs given should match those used in the template above. */
+  (function() {{
+    var img_id = "_anim_img{id}";
+    var slider_id = "_anim_slider{id}";
+    var loop_select_id = "_anim_loop_select{id}";
+    var frames = new Array({Nframes});
+    {fill_frames}
+    /* set a timeout to make sure all the above elements are created before
+       the object is initialized. */
+    setTimeout(function() {{
+        anim{id} = new Animation(frames, img_id, slider_id, {interval},
+                                 loop_select_id);
+    }}, 0);
+  }})()
+</script>
+"""
+
+
+INCLUDED_FRAMES = """
+  for (var i=0; i<{Nframes}; i++){{
+    frames[i] = "{frame_dir}/frame" + ("0000000" + i).slice(-7) +
+                ".{frame_format}";
+  }}
+"""
