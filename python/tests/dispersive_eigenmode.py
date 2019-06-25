2d6aa85d19d65b25b84f87c4f3ee08c0d2b7cd91
Alec Hammond <alec.m.hammond@gmail.com>
2019-06-25 09:36:21 -0600

    cleanup

--- python/tests/dispersive_eigenmode.py
+++ python/tests/dispersive_eigenmode.py
@@ -17,7 +17,7 @@ class TestDispersiveEigenmode(unittest.TestCase):
     # ----------------------------------------- #
     # ----------- Helper Functions ------------ #
     # ----------------------------------------- #
-    # Directly calss the C++ chi1 routine
+    # Directly cals the C++ chi1 routine
     def call_chi1(self,material,omega):
 
         sim = mp.Simulation(cell_size=mp.Vector3(1,1,1),
@@ -35,48 +35,14 @@ class TestDispersiveEigenmode(unittest.TestCase):
         n_actual = np.real(np.sqrt(material.epsilon(omega).astype(np.complex128)))
         
         np.testing.assert_allclose(n,n_actual)
-
-    # Pulls the "effective index" of a uniform, dispersive material
-    # (i.e. the refractive index) using meep's get_eigenmode
-    def simulate_meep(self,material,omega):
-        
-        sim = mp.Simulation(cell_size=mp.Vector3(1,1,1),
-                            default_material=material,
-                            resolution=10
-                            )
-
-        band_num = 1
-        sim.init_sim()
-
-        # Pull the x direction
-        direction = mp.X
-        where = mp.Volume(center=mp.Vector3(0,0,0),size=mp.Vector3(0,0,0))
-        kpoint = mp.Vector3(1,0,0)
-        emx = sim.get_eigenmode(omega,direction,where,band_num,kpoint)
-        
-        # Pull the y direction
-        direction = mp.Y
-        where = mp.Volume(center=mp.Vector3(0,0,0),size=mp.Vector3(0,0,0))
-        kpoint = mp.Vector3(0,1,0)
-        emy = sim.get_eigenmode(omega,direction,where,band_num,kpoint)
-
-        # Pull the z direction
-        direction = mp.Z
-        where = mp.Volume(center=mp.Vector3(0,0,0),size=mp.Vector3(0,0,0))
-        kpoint = mp.Vector3(0,0,1)
-        emz = sim.get_eigenmode(omega,direction,where,band_num,kpoint)
-        
-        # combine and return
-        k = np.array(mp.Matrix(emx.k,emy.k,emz.k))
-        neff_meep = np.squeeze(k) / omega
-
-        n = np.real(np.sqrt(material.epsilon(omega).astype(np.complex128)))
-
-        np.testing.assert_allclose(n,neff_meep)
     
     def verify_output_and_slice(self,material,omega):
         # Since the slice routines average the diagonals, we need to do that too:
-        n = np.real(np.sqrt(np.mean(np.linalg.eigvals(material.epsilon(omega)))))
+        chi1 = np.square(np.real(np.sqrt(material.epsilon(omega))))
+        chi1inv = (np.linalg.inv(chi1))
+        chi1inv = np.diag(chi1inv)
+        N = chi1inv.size
+        n = np.sqrt(N/np.sum(chi1inv))
         
         sim = mp.Simulation(cell_size=mp.Vector3(2,2,2),
                             default_material=material,
@@ -130,30 +96,12 @@ class TestDispersiveEigenmode(unittest.TestCase):
         self.call_chi1(LiNbO3,w0)
         self.call_chi1(LiNbO3,w1)
 
-        LiNbO3.rotate([mp.Vector3(x=np.radians(28)),mp.Vector3(np.radians(45))])
-        self.call_chi1(LiNbO3,w0)
-        self.call_chi1(LiNbO3,w1)
-    
-    def atest_meep_eigenmode(self):
-        # Checks the get_eigenmode features with dispersive materials
-        # NOTE: metals are not supported
-        from meep.materials import Si, Ag, LiNbO3, Au
-
-        # Check Silicon
-        w0 = Si.valid_freq_range.min
-        w1 = Si.valid_freq_range.max
-        self.simulate_meep(Si,w0)
-        self.simulate_meep(Si,w1)
-
-        # Check Lithium Niobate
-        w0 = LiNbO3.valid_freq_range.min
-        w1 = LiNbO3.valid_freq_range.max
-        #self.simulate_meep(LiNbO3,w0)
-        #self.simulate_meep(LiNbO3,w1)
-
-        LiNbO3.rotate([mp.Vector3(x=np.radians(28)),mp.Vector3(np.radians(45))])
-        #self.simulate_meep(LiNbO3,w0)
-        #self.simulate_meep(LiNbO3,w1)
+        # Now let's rotate LN
+        import copy
+        rotLiNbO3 = copy.deepcopy(LiNbO3)
+        rotLiNbO3.rotate([mp.Vector3(x=np.radians(28)),mp.Vector3(np.radians(45))])
+        self.call_chi1(rotLiNbO3,w0)
+        self.call_chi1(rotLiNbO3,w1)
     
     def test_get_with_dispersion(self):
         # Checks the get_array_slice and output_fields method
@@ -182,8 +130,15 @@ class TestDispersiveEigenmode(unittest.TestCase):
         # Check Lithium Niobate
         w0 = LiNbO3.valid_freq_range.min
         w1 = LiNbO3.valid_freq_range.max
-        #self.verify_output_and_slice(LiNbO3,w0)
-        #self.verify_output_and_slice(LiNbO3,w1)
+        self.verify_output_and_slice(LiNbO3,w0)
+        self.verify_output_and_slice(LiNbO3,w1)
+
+        # Now let's rotate LN
+        import copy
+        rotLiNbO3 = copy.deepcopy(LiNbO3)
+        rotLiNbO3.rotate([mp.Vector3(x=np.radians(28)),mp.Vector3(np.radians(45))])
+        self.verify_output_and_slice(rotLiNbO3,w0)
+        self.verify_output_and_slice(rotLiNbO3,w1)
         
 
 if __name__ == '__main__':
