6aba910bfa57812564db1c8d9bb17aa6a5dd57ac
Ardavan Oskooi <ardavan.oskooi@gmail.com>
2019-07-01 21:12:48 -0700

    tutorial example for mode decomposition of oblique waveguide

--- python/examples/oblique-source.py
+++ python/examples/oblique-source.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 resolution = 50 # pixels/μm
 
-cell_size = mp.Vector3(14,14,0)
+cell_size = mp.Vector3(14,14)
 
 pml_layers = [mp.PML(thickness=2)]
 
@@ -21,6 +21,8 @@ fsrc = 0.15 # frequency of eigenmode or constant-amplitude source
 kx = 0.4    # initial guess for wavevector in x-direction of eigenmode
 bnum = 1    # band number of eigenmode
 
+kpoint = mp.Vector3(kx).rotate(mp.Vector3(z=1), rot_angle)
+
 compute_flux = True # compute flux (True) or plot the field profile (False)
 
 eig_src = True # eigenmode (True) or constant-amplitude (False) source
@@ -29,8 +31,8 @@ if eig_src:
     sources = [mp.EigenModeSource(src=mp.GaussianSource(fsrc,fwidth=0.2*fsrc) if compute_flux else mp.ContinuousSource(fsrc),
                                   center=mp.Vector3(),
                                   size=mp.Vector3(y=14),
-                                  direction=mp.AUTOMATIC if rot_angle == 0 else mp.NO_DIRECTION,
-                                  eig_kpoint=mp.Vector3(kx).rotate(mp.Vector3(z=1), rot_angle),
+                                  direction=mp.NO_DIRECTION,
+                                  eig_kpoint=kpoint,
                                   eig_band=bnum,
                                   eig_parity=mp.EVEN_Y+mp.ODD_Z if rot_angle == 0 else mp.ODD_Z,
                                   eig_match_freq=True)]
@@ -50,14 +52,15 @@ sim = mp.Simulation(cell_size=cell_size,
 if compute_flux:
     tran = sim.add_flux(fsrc, 0, 1, mp.FluxRegion(center=mp.Vector3(x=5), size=mp.Vector3(y=14)))
     sim.run(until_after_sources=50)
-    print("flux:, {:.6f}".format(mp.get_fluxes(tran)[0]))
+    res = sim.get_eigenmode_coefficients(tran,
+                                         [1],
+                                         eig_parity=mp.EVEN_Y+mp.ODD_Z if rot_angle == 0 else mp.ODD_Z,
+                                         direction=mp.NO_DIRECTION,
+                                         kpoint_func=lambda f,n: kpoint)
+    print("flux:, {:.6f}, {:.6f}".format(mp.get_fluxes(tran)[0],abs(res.alpha[0,0,0])**2))
 else:
     sim.run(until=100)
-    nonpml_vol = mp.Volume(center=mp.Vector3(), size=mp.Vector3(10,10,0))
-    eps_data = sim.get_array(vol=nonpml_vol, component=mp.Dielectric)
-    ez_data = sim.get_array(vol=nonpml_vol, component=mp.Ez)
-    plt.figure()
-    plt.imshow(np.flipud(np.transpose(eps_data)), interpolation='spline36', cmap='binary')
-    plt.imshow(np.flipud(np.transpose(ez_data)), interpolation='spline36', cmap='RdBu', alpha=0.9)
-    plt.axis('off')
+    sim.plot2D(output_plane=mp.Volume(center=mp.Vector3(), size=mp.Vector3(10,10)),
+               fields=mp.Ez,
+               field_parameters={'alpha':0.9})
     plt.show()
