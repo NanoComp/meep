f2ff5681664ce7566de5fa19031e1ab2884eb88b
Ardavan Oskooi <oskooi@users.noreply.github.com>
2019-07-02 10:52:42 -0700

    tutorial example for mode decomposition of oblique waveguide (#942)

--- python/tests/oblique_source.py
+++ python/tests/oblique_source.py
@@ -7,7 +7,7 @@ import unittest
 class TestEigenmodeSource(unittest.TestCase):
 
     def test_waveguide_flux(self):
-        cell_size = mp.Vector3(10,10,0)
+        cell_size = mp.Vector3(10,10)
 
         pml_layers = [mp.PML(thickness=2.0)]
 
@@ -29,8 +29,8 @@ class TestEigenmodeSource(unittest.TestCase):
 
             geometry = [mp.Block(center=mp.Vector3(),
                                  size=mp.Vector3(mp.inf,1,mp.inf),
-                                 e1 = mp.Vector3(1,0,0).rotate(mp.Vector3(0,0,1), rot_angle),
-                                 e2 = mp.Vector3(0,1,0).rotate(mp.Vector3(0,0,1), rot_angle),
+                                 e1 = mp.Vector3(1).rotate(mp.Vector3(z=1), rot_angle),
+                                 e2 = mp.Vector3(y=1).rotate(mp.Vector3(z=1), rot_angle),
                                  material=mp.Medium(index=1.5))]
 
             sim = mp.Simulation(cell_size=cell_size,
@@ -43,9 +43,11 @@ class TestEigenmodeSource(unittest.TestCase):
 
             sim.run(until_after_sources=100)
 
-            res = sim.get_eigenmode_coefficients(tran, [1],
-                    eig_parity=mp.ODD_Z, direction=mp.NO_DIRECTION,
-                    kpoint_func=lambda f,n: kpoint)
+            res = sim.get_eigenmode_coefficients(tran,
+                                                 [1],
+                                                 eig_parity=mp.EVEN_Y+mp.ODD_Z if t == 0 else mp.ODD_Z,
+                                                 direction=mp.NO_DIRECTION,
+                                                 kpoint_func=lambda f,n: kpoint)
 
             fluxes.append(mp.get_fluxes(tran)[0])
             coeff_fluxes.append(abs(res.alpha[0,0,0])**2)
