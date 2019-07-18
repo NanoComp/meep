e58dcf2494c2e34b03cb00909b59dbeabf751737
David Gauchard <gauchard@laas.fr>
2019-07-18 11:41:29 +0200

    automate full installation to any directory (not only /usr/local)
        This script is very useful for
    
        - users with electromagnetics skills in the need for the latest MEEP developments
          but lacking time to get sufficient system knowledge to reach their grail,
    
        - system administrators constantly being asked for the latest MEEP versions by
          their impatient electromagnetics specialists users (sometimes for years)
    
        This script has his place right in the source repository, so the (large)
        audience can improve and adapt it over time.
    
        Based on https://github.com/NanoComp/meep/blob/9f68f390f68ecb8a18d98f51c8166c808a1e6f98/doc/docs/Build_From_Source.md#ubuntu-1604-and-1804
        Build for ubuntu16.04, 18.04 and CentOS 7

--- contrib/build-meep.sh
+++ contrib/build-meep.sh
@@ -23,15 +23,23 @@ read junk
 
 set -ex
 
-distrib=$(lsb_release -c -s)
+ubuntu=false
+centos=false
+
+distrib=$(lsb_release -r -s)
 case "$distrib" in
-    bionic) # ubuntu 18.04
+    18.04) # ubuntu 18.04 bionic
         libpng=libpng-dev
         libpython=libpython3-dev
+        ubuntu=true
         ;;
-    xenial) # ubuntu 16.04
+    16.04) # ubuntu 16.04 xenal
         libpng=libpng16-dev
         libpython=libpython3.5-dev
+        ubuntu=true
+        ;;
+    7.*) # CentOS 7.x
+        centos=true
         ;;
     *)
         echo "unsupported distribution '$(lsb_release -a)', edit and fix!"
@@ -74,39 +82,97 @@ autogensh ()
         "$@"
 }
 
-sudo apt-get update
+if $ubuntu; then
 
-# If building on Ubuntu 18.04LTS, replace libpng16-dev with libpng-dev,
-# and libpython3.5-dev with libpython3-dev.
-sudo apt-get -y install     \
-    build-essential         \
-    gfortran                \
-    libblas-dev             \
-    liblapack-dev           \
-    libgmp-dev              \
-    swig                    \
-    libgsl-dev              \
-    autoconf                \
-    pkg-config              \
-    $libpng                 \
-    git                     \
-    guile-2.0-dev           \
-    libfftw3-dev            \
-    libhdf5-openmpi-dev     \
-    hdf5-tools              \
-    $libpython              \
-    python3-numpy           \
-    python3-scipy           \
-    python3-pip             \
-    ffmpeg                  \
+    sudo apt-get; update
 
-# The next line is only required on Ubuntu  16.04
-[ "$distrib" = xenial ] && sudo -H pip3 install --upgrade pip
+    # If building on Ubuntu 18.04LTS, replace libpng16-dev with libpng-dev,
+    # and libpython3.5-dev with libpython3-dev.
+    sudo apt-get -y install     \
+        build-essential         \
+        gfortran                \
+        libblas-dev             \
+        liblapack-dev           \
+        libgmp-dev              \
+        swig                    \
+        libgsl-dev              \
+        autoconf                \
+        pkg-config              \
+        $libpng                 \
+        git                     \
+        guile-2.0-dev           \
+        libfftw3-dev            \
+        libhdf5-openmpi-dev     \
+        hdf5-tools              \
+        $libpython              \
+        python3-numpy           \
+        python3-scipy           \
+        python3-pip             \
+        ffmpeg                  \
 
-sudo -H pip3 install --no-cache-dir mpi4py
-export HDF5_MPI="ON"
-sudo -H pip3 install --no-binary=h5py h5py
-sudo -H pip3 install matplotlib>3.0.0
+    # The next line is only required on Ubuntu  16.04
+    [ "$distrib" = xenial ] && sudo -H pip3 install --upgrade pip
+
+    sudo -H pip3 install --no-cache-dir mpi4py
+    export HDF5_MPI="ON"
+    sudo -H pip3 install --no-binary=h5py h5py
+    sudo -H pip3 install matplotlib>3.0.0
+fi
+
+if $centos; then
+
+    sudo yum -y --enablerepo=extras install epel-release
+
+    sudo yum -y install   \
+        bison             \
+        byacc             \
+        cscope            \
+        ctags             \
+        cvs               \
+        diffstat          \
+        oxygen            \
+        flex              \
+        gcc               \
+        gcc-c++           \
+        gcc-gfortran      \
+        gettext           \
+        git               \
+        indent            \
+        intltool          \
+        libtool           \
+        patch             \
+        patchutils        \
+        rcs               \
+        redhat-rpm-config \
+        rpm-build         \
+        subversion        \
+        systemtap         \
+        wget
+
+    sudo yum -y install    \
+        openblas-devel     \
+        fftw3-devel        \
+        libpng-devel       \
+        gsl-devel          \
+        gmp-devel          \
+        pcre-devel         \
+        libtool-ltdl-devel \
+        libunistring-devel \
+        libffi-devel       \
+        gc-devel           \
+        zlib-devel         \
+        openssl-devel      \
+        sqlite-devel       \
+        bzip2-devel        \
+        ffmpeg
+
+    sudo yum -y install    \
+        openmpi-devel      \
+        hdf5-openmpi-devel \
+        guile-devel
+
+    export PATH=${PATH}:/usr/lib64/openmpi/bin
+fi
 
 mkdir -p $SRCDIR
 
