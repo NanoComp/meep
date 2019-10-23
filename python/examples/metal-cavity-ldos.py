f65f68c2c1baa4bd04a91a6d2052281ec2916552
Ardavan Oskooi <oskooi@users.noreply.github.com>
2019-10-22 18:02:13 -0700

    Jupyter notebooks for optical forces and local density of states tutorial examples (#1037)
    * Jupyter notebook for optical forces tutorial example
    
    * Jupyter notebook for local density of states (LDOS) tutorial example

--- python/examples/metal-cavity-ldos.py
+++ python/examples/metal-cavity-ldos.py
@@ -2,58 +2,69 @@ from __future__ import division
 
 import math
 import meep as mp
-import argparse
+import numpy as np
+import matplotlib.pyplot as plt
 
-def main(args):
-    resolution = 200
+def metal_cavity(w):
+    resolution = 50
     sxy = 2
     dpml = 1
-    sxy = sxy + 2 * dpml
-    cell = mp.Vector3(sxy, sxy, 0)
+    sxy = sxy+2*dpml
+    cell = mp.Vector3(sxy,sxy)
 
     pml_layers = [mp.PML(dpml)]
     a = 1
     t = 0.1
-    geometry = [mp.Block(mp.Vector3(a + 2 * t, a + 2 * t, mp.inf), material=mp.metal),
-                mp.Block(mp.Vector3(a, a, mp.inf), material=mp.air)]
+    geometry = [mp.Block(mp.Vector3(a+2*t,a+2*t,mp.inf), material=mp.metal),
+                mp.Block(mp.Vector3(a,a,mp.inf), material=mp.air)]
 
-    w = args.w
-    if w > 0:
-        geometry.append(mp.Block(center=mp.Vector3(a / 2), size=mp.Vector3(2 * t, w, mp.inf),
-                                 material=mp.air))
+    geometry.append(mp.Block(center=mp.Vector3(a/2),
+                             size=mp.Vector3(2*t,w,mp.inf),
+                             material=mp.air))
 
-        fcen = math.sqrt(0.5) / a
-        df = 0.2
-        sources = [mp.Source(src=mp.GaussianSource(fcen, fwidth=df), component=mp.Ez,
-                             center=mp.Vector3())]
+    fcen = math.sqrt(0.5)/a
+    df = 0.2
+    sources = [mp.Source(src=mp.GaussianSource(fcen,fwidth=df),
+                         component=mp.Ez,
+                         center=mp.Vector3())]
 
-        symmetries = [mp.Mirror(mp.Y)]
+    symmetries = [mp.Mirror(mp.Y)]
 
-        Th = args.Th
+    sim = mp.Simulation(cell_size=cell,
+                        geometry=geometry,
+                        boundary_layers=pml_layers,
+                        sources=sources,
+                        symmetries=symmetries,
+                        resolution=resolution)
 
-        sim = mp.Simulation(cell_size=cell,
-                            geometry=geometry,
-                            boundary_layers=pml_layers,
-                            sources=sources,
-                            symmetries=symmetries,
-                            resolution=resolution)
+    h = mp.Harminv(mp.Ez, mp.Vector3(), fcen, df)
+    sim.run(mp.after_sources(h), until_after_sources=500)
 
-        h = mp.Harminv(mp.Ez, mp.Vector3(), fcen, df)
-        sim.run(mp.after_sources(h), until_after_sources=Th)
+    m = h.modes[0]
+    f = m.freq
+    Q = m.Q
+    Vmode = 0.25*a*a
+    ldos_1 = Q / Vmode / (2 * math.pi * f * math.pi * 0.5)
 
-        m = h.modes[0]
-        f = m.freq
-        Q = m.Q
-        Vmode = 0.25 * a * a
-        print("ldos0:, {}".format(Q / Vmode / (2 * math.pi * f * math.pi * 0.5)))
+    sim.reset_meep()
 
-        sim.reset_meep()
-        T = 2 * Q * (1 / f)
-        sim.run(mp.dft_ldos(f, 0, 1), until_after_sources=T)
+    T = 2*Q*(1/f)
+    sim.run(mp.dft_ldos(f,0,1), until_after_sources=T)
+    ldos_2 = sim.ldos_data[0]
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-Th', type=float, default=500, help='additional time after source has turned off to accumulate Harminv data')
-    parser.add_argument('-w', type=float, default=0, help='width of cavity opening')
-    args = parser.parse_args()
-    main(args)
+    return ldos_1, ldos_2
+
+ws = np.arange(0.2,0.5,0.1)
+ldos_1 = np.zeros(len(ws))
+ldos_2 = np.zeros(len(ws))
+
+for j in range(len(ws)):
+    ldos_1[j], ldos_2[j] = metal_cavity(ws[j])
+    print("ldos:, {}, {}".format(ldos_1[j],ldos_2[2]))
+
+plt.figure(dpi=150)
+plt.semilogy(1/ws,ldos_1,'bo-',label="2Q/(πωV)")
+plt.semilogy(1/ws,ldos_2,'rs-',label="LDOS")
+plt.xlabel('a/w')
+plt.ylabel('2Q/(πωW) or LDOS')
+plt.show()
