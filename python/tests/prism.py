a914f705ef1e2e5156fad18f97140d96cf72f8b5
Ardavan Oskooi <ardavan.oskooi@gmail.com>
2020-02-26 21:38:20 -0800

    add test involving marching squares algorithm

--- python/tests/prism.py
+++ python/tests/prism.py
@@ -1,16 +1,66 @@
 from __future__ import division
 
+import os
 import unittest
 import numpy as np
 import meep as mp
 
 class TestPrism(unittest.TestCase):
 
-  def prism_cyl_eps(self,npts,r):
-    resolution = 70
+  def prism_marching_squares(self,npts):
+    resolution = 50
 
     cell = mp.Vector3(3,3)
 
+    data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data'))
+    vertices_file = os.path.join(data_dir, 'prism_vertices.npz')
+    vertices_obj = np.load(vertices_file)
+
+    ## prism vertices precomputed for a circle of radius 1 using
+    ## marching squares algorithm of skimage.measure.find_contours
+    ## ref: https://github.com/NanoComp/meep/issues/1060
+    vertices_data = vertices_obj["N{}".format(npts)]
+    vertices = [mp.Vector3(v[0],v[1],0) for v in vertices_data]
+
+    geometry = [mp.Prism(vertices,
+                         height=mp.inf,
+                         material=mp.Medium(epsilon=12))]
+
+    sim = mp.Simulation(cell_size=cell,
+                        geometry=geometry,
+                        resolution=resolution)
+
+    sim.init_sim()
+
+    prism_eps = sim.integrate_field_function([mp.Dielectric], lambda r,eps: eps)
+
+    sim.reset_meep()
+
+    geometry = [mp.Cylinder(radius=1.0,
+                            center=mp.Vector3(),
+                            height=mp.inf,
+                            material=mp.Medium(epsilon=12))]
+
+    sim = mp.Simulation(cell_size=cell,
+                        geometry=geometry,
+                        resolution=resolution)
+
+    sim.init_sim()
+
+    cyl_eps = sim.integrate_field_function([mp.Dielectric], lambda r,eps: eps)
+
+    print("epsilon-sum:, {} (prism-msq), {} (cylinder), {} (relative error)".format(abs(prism_eps),abs(cyl_eps),abs(prism_eps-cyl_eps)/abs(cyl_eps)))
+
+    return abs(prism_eps-cyl_eps)/abs(cyl_eps)
+
+
+  def prism_circle(self,npts,r):
+    resolution = 50
+
+    cell = mp.Vector3(3,3)
+
+    ### prism vertices computed as npts equally-spaced points
+    ### along the circumference of a circle with radius r
     angles = 2*np.pi/npts * np.arange(npts)
     vertices = [mp.Vector3(r*np.cos(ang),r*np.sin(ang)) for ang in angles]
     geometry = [mp.Prism(vertices,
@@ -40,29 +90,36 @@ class TestPrism(unittest.TestCase):
 
     cyl_eps = sim.integrate_field_function([mp.Dielectric], lambda r,eps: eps)
 
-    if mp.am_master():
-      print("epsilon:, {} (prism), {} (cylinder), {} (relative error)".format(abs(prism_eps),abs(cyl_eps),abs(prism_eps-cyl_eps)/abs(cyl_eps)))
+    print("epsilon-sum:, {} (prism-cyl), {} (cylinder), {} (relative error)".format(abs(prism_eps),abs(cyl_eps),abs(prism_eps-cyl_eps)/abs(cyl_eps)))
 
     return abs(prism_eps-cyl_eps)/abs(cyl_eps)
 
 
   def test_prism(self):
-    r = 1.1958710786934182
-    d = [self.prism_cyl_eps(50,r),
-         self.prism_cyl_eps(100,r),
-         self.prism_cyl_eps(200,r)]
+    print("Testing Prism object using marching squares algorithm...")
+    d = [self.prism_marching_squares(92),
+         self.prism_marching_squares(192),
+         self.prism_marching_squares(392)]
+
+    self.assertLess(d[1],d[0])
+    self.assertLess(d[2],d[1])
+
+    print("Testing Prism object using circle formula...")
+    r = 1.0458710786934182
+    d = [self.prism_circle(51,r),
+         self.prism_circle(101,r),
+         self.prism_circle(201,r)]
 
     self.assertLess(d[1],d[0])
     self.assertLess(d[2],d[1])
 
     r = 1.2896871096581341
-    d = [self.prism_cyl_eps(22,r),
-         self.prism_cyl_eps(44,r),
-         self.prism_cyl_eps(88,r)]
+    d = [self.prism_circle(31,r),
+         self.prism_circle(61,r),
+         self.prism_circle(121,r)]
 
     self.assertLess(d[1],d[0])
-    self.assertLess(d[2],d[1])    
-
+    self.assertLess(d[2],d[1])
 
 if __name__ == '__main__':
   unittest.main()
