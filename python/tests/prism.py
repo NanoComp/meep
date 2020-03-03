bc30a25f3d4245cd66c9c1340e8c746f1a04f0e1
Ardavan Oskooi <ardavan.oskooi@gmail.com>
2020-03-02 16:31:12 -0800

    Python test for nonconvex prisms

--- python/tests/prism.py
+++ python/tests/prism.py
@@ -7,13 +7,44 @@ import meep as mp
 
 class TestPrism(unittest.TestCase):
 
-  def prism_marching_squares(self,npts):
+  def nonconvex_marching_squares(self,idx,npts):
+    resolution = 15
+
+    cell = mp.Vector3(10,10)
+
+    data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data'))
+    vertices_file = os.path.join(data_dir, 'nonconvex_prism_vertices{}.npz'.format(idx))
+    vertices_obj = np.load(vertices_file)
+
+    ## prism verticies precomputed from analytic blob shape using
+    ## marching squares algorithm of skimage.measure.find_contours
+    vertices_data = vertices_obj["N{}".format(npts)]
+    vertices = [mp.Vector3(v[0],v[1],0) for v in vertices_data]
+
+    geometry = [mp.Prism(vertices,
+                         height=mp.inf,
+                         material=mp.Medium(epsilon=12))]
+
+    sim = mp.Simulation(cell_size=cell,
+                        geometry=geometry,
+                        resolution=resolution)
+
+    sim.init_sim()
+
+    prism_eps = sim.integrate_field_function([mp.Dielectric], lambda r,eps: eps)
+
+    print("epsilon-sum:, {} (prism-msq)".format(abs(prism_eps)))
+
+    return prism_eps
+
+
+  def convex_marching_squares(self,npts):
     resolution = 50
 
     cell = mp.Vector3(3,3)
 
     data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data'))
-    vertices_file = os.path.join(data_dir, 'prism_vertices.npz')
+    vertices_file = os.path.join(data_dir, 'convex_prism_vertices.npz')
     vertices_obj = np.load(vertices_file)
 
     ## prism vertices precomputed for a circle of radius 1.0 using
@@ -54,12 +85,12 @@ class TestPrism(unittest.TestCase):
     return abs((prism_eps-cyl_eps)/cyl_eps)
 
 
-  def prism_circle(self,npts,r):
+  def convex_circle(self,npts,r):
     resolution = 50
 
     cell = mp.Vector3(3,3)
 
-    ### prism vertices computed as npts equally-spaced points
+    ### prism vertices computed as equally-spaced points
     ### along the circumference of a circle with radius r
     angles = 2*np.pi/npts * np.arange(npts)
     vertices = [mp.Vector3(r*np.cos(ang),r*np.sin(ang)) for ang in angles]
@@ -96,27 +127,49 @@ class TestPrism(unittest.TestCase):
 
 
   def test_prism(self):
-    print("Testing Prism object using marching squares algorithm...")
-    d = [self.prism_marching_squares(92),
-         self.prism_marching_squares(192),
-         self.prism_marching_squares(392)]
+    print("Testing Non-Convex Prism #1 using marching squares algorithm...")
+    d1_a = self.nonconvex_marching_squares(1,208)
+    d1_b = self.nonconvex_marching_squares(1,448)
+    d1_ref = self.nonconvex_marching_squares(1,904)
+
+    self.assertLess(abs((d1_b-d1_ref)/d1_ref),abs((d1_a-d1_ref)/d1_ref))
+
+    print("Testing Non-Convex Prism #2 using marching squares algorithm...")
+    d2_a = self.nonconvex_marching_squares(2,128)
+    d2_b = self.nonconvex_marching_squares(2,256)
+    d2_ref = self.nonconvex_marching_squares(2,516)
+
+    self.assertLess(abs((d2_b-d2_ref)/d2_ref),abs((d2_a-d2_ref)/d2_ref))
+
+    print("Testing Non-Convex Prism #3 using marching squares algorithm...")
+    d3_a = self.nonconvex_marching_squares(3,164)
+    d3_b = self.nonconvex_marching_squares(3,336)
+    d3_ref = self.nonconvex_marching_squares(3,672)
+
+    self.assertLess(abs((d3_b-d3_ref)/d3_ref),abs((d3_a-d3_ref)/d3_ref))
+
+    print("Testing Convex Prism using marching squares algorithm...")
+    d = [self.convex_marching_squares(92),
+         self.convex_marching_squares(192),
+         self.convex_marching_squares(392)]
 
     self.assertLess(d[1],d[0])
     self.assertLess(d[2],d[1])
 
-    print("Testing Prism object using circle formula...")
+    print("Testing Convex Prism #1 using circle formula...")
     r = 1.0458710786934182
-    d = [self.prism_circle(51,r),
-         self.prism_circle(101,r),
-         self.prism_circle(201,r)]
+    d = [self.convex_circle(51,r),
+         self.convex_circle(101,r),
+         self.convex_circle(201,r)]
 
     self.assertLess(d[1],d[0])
     self.assertLess(d[2],d[1])
 
+    print("Testing Convex Prism #2 using circle formula...")
     r = 1.2896871096581341
-    d = [self.prism_circle(31,r),
-         self.prism_circle(61,r),
-         self.prism_circle(121,r)]
+    d = [self.convex_circle(31,r),
+         self.convex_circle(61,r),
+         self.convex_circle(121,r)]
 
     self.assertLess(d[1],d[0])
     self.assertLess(d[2],d[1])
