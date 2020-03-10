1c6ee967db38085ebbc37dbfa66229c5e761921e
Ardavan Oskooi <ardavan.oskooi@gmail.com>
2020-03-10 15:17:43 -0700

    Python test for GDSII prisms

--- python/tests/prism.py
+++ python/tests/prism.py
@@ -128,6 +128,27 @@ class TestPrism(unittest.TestCase):
 
     return abs((prism_eps-cyl_eps)/cyl_eps)
 
+  def spiral_gds(self):
+    data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data'))
+    gdsii_file = os.path.join(data_dir, 'spiral.gds')
+
+    resolution = 25
+    cell_size = mp.Vector3(10,16)
+    geometry_center = mp.Vector3(1.5,7.3)
+    geometry = mp.get_GDSII_prisms(mp.Medium(index=3.5), gdsii_file, 0, 0, mp.inf)
+
+    sim = mp.Simulation(cell_size=cell_size,
+                        geometry=geometry,
+                        geometry_center=geometry_center,
+                        resolution=resolution)
+
+    sim.init_sim()
+
+    prism_eps = sim.integrate_field_function([mp.Dielectric], lambda r,eps: eps)
+
+    print("epsilon-sum:, {} (prism-gds)".format(abs(prism_eps)))
+
+    return prism_eps
 
   def test_prism(self):
     print("Testing Non-Convex Prism #1 using marching squares algorithm...")
@@ -200,6 +221,11 @@ class TestPrism(unittest.TestCase):
     self.assertAlmostEqual(d_nosym[0],d_sym[0],places=3)
     self.assertAlmostEqual(d_nosym[1],d_sym[1],places=3)
     self.assertAlmostEqual(d_nosym[2],d_sym[2],places=3)
+
+    print("Testing Non-Convex Prism from GDSII file...")
+    d = self.spiral_gds()
+    d_ref = 423.2359686656507
+    self.assertAlmostEqual(d,d_ref,places=5)
 
 if __name__ == '__main__':
   unittest.main()
