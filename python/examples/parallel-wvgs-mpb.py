3633acfb191d1f4e8797730fe1474be2f460aff1
Ardavan Oskooi <ardavan.oskooi@gmail.com>
2020-05-12 18:27:38 -0700

    update Jupyter notebook and Scheme version of optical forces tutorial (#1214)
    * update Jupyter notebook and Scheme version of optical forces tutorial
    
    * new figure showing mode profile

--- python/examples/parallel-wvgs-mpb.py
+++ python/examples/parallel-wvgs-mpb.py
@@ -41,7 +41,7 @@ def parallel_waveguide(s,yodd):
 
     return f,vg
 
-ss = np.arange(0.05,1.05,0.05)
+ss = np.arange(0.025,1.075,0.05)
 
 f_odd = np.zeros(len(ss))
 vg_odd = np.zeros(len(ss))
@@ -58,13 +58,13 @@ def compute_force(f,vg):
     f_avg = 0.5*(f[:-1]+f[1:])
     df = f[1:]-f[:-1]
     vg_avg = 0.5*(vg[:-1]+vg[1:])
-    return np.multiply(np.multiply(-1/f_avg,df/ds), 1/vg_avg)
+    return -1/f_avg * df/ds * 1/vg_avg
 
 force_odd = compute_force(f_odd,vg_odd)
 force_even = compute_force(f_even,vg_even)
 
 plt.figure(dpi=200)
-plt.plot(ss[:-1],force_odd,'b-',label='antisymmetric')
+plt.plot(ss[:-1],force_odd,'b-',label='anti-symmetric')
 plt.plot(ss[:-1],force_even,'r-',label='symmetric')
 plt.xlabel("waveguide separation s/a")
 plt.ylabel("optical force (F/L)(ac/P)")
