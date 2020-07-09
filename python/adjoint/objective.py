fix gradients

python/adjoint/objective.py
@@ -35,15 +35,15 @@ class EigenmodeCoefficient(ObjectiveQuantitiy):
         self.eval = None
         self.EigenMode_kwargs = kwargs
         return
-    
+
     def register_monitors(self,frequencies):
         self.frequencies = np.asarray(frequencies)
         self.monitor = self.sim.add_mode_monitor(frequencies,mp.FluxRegion(center=self.volume.center,size=self.volume.size),yee_grid=True)
         self.normal_direction = self.monitor.normal_direction
         return self.monitor
-    
+
     def place_adjoint_source(self,dJ):
-        '''Places an equivalent eigenmode monitor facing the opposite direction. Calculates the 
+        '''Places an equivalent eigenmode monitor facing the opposite direction. Calculates the
         correct scaling/time profile.
 
         dJ ........ the user needs to pass the dJ/dMonitor evaluation
@@ -64,14 +64,14 @@ class EigenmodeCoefficient(ObjectiveQuantitiy):
                 k0 == direction_scalar * mp.Vector3(z=1)
         else:
             k0 = direction_scalar * self.kpoint_func(self.time_src.frequency,1)
-        
+
         # -------------------------------------- #
-        # Get scaling factor 
+        # Get scaling factor
         # -------------------------------------- #
         # leverage linearity and combine source for multiple frequencies
         if dJ.ndim == 2:
             dJ = np.sum(dJ,axis=1)
-        
+
         # Determine the correct resolution scale factor
         if self.sim.cell_size.y == 0:
             dV = 1/self.sim.resolution
@@ -79,16 +79,16 @@ class EigenmodeCoefficient(ObjectiveQuantitiy):
             dV = 1/self.sim.resolution * 1/self.sim.resolution
         else:
             dV = 1/self.sim.resolution * 1/self.sim.resolution * 1/self.sim.resolution
-        
+
         da_dE = 0.5 * self.cscale # scalar popping out of derivative
         iomega = (1.0 - np.exp(-1j * (2 * np.pi * self.frequencies) * dt)) * (1.0 / dt) # scaled frequency factor with discrete time derivative fix
 
         src = self.time_src
-        
+
         # an ugly way to calcuate the scaled dtft of the forward source
         y = np.array([src.swigobj.current(t,dt) for t in np.arange(0,T,dt)]) # time domain signal
         fwd_dtft = np.matmul(np.exp(1j*2*np.pi*self.frequencies[:,np.newaxis]*np.arange(y.size)*dt), y)*dt/np.sqrt(2*np.pi) # dtft
-        
+
         # we need to compensate for the phase added by the time envelope at our freq of interest
         src_center_dtft = np.matmul(np.exp(1j*2*np.pi*np.array([src.frequency])[:,np.newaxis]*np.arange(y.size)*dt), y)*dt/np.sqrt(2*np.pi)
         adj_src_phase = np.exp(1j*np.angle(src_center_dtft))
@@ -101,7 +101,7 @@ class EigenmodeCoefficient(ObjectiveQuantitiy):
             scale = da_dE * dV * dJ * iomega / adj_src_phase
             src = FilteredSource(self.time_src.frequency,self.frequencies,scale,dt) # generate source from broadband response
             amp = 1
-        
+
         # generate source object
         self.source = [mp.EigenModeSource(src,
                     eig_band=self.mode,
@@ -112,7 +112,7 @@ class EigenmodeCoefficient(ObjectiveQuantitiy):
                     size=self.volume.size,
                     center=self.volume.center,
                     **self.EigenMode_kwargs)]
-        
+
         return self.source
 
     def __call__(self):
@@ -122,7 +122,7 @@ class EigenmodeCoefficient(ObjectiveQuantitiy):
         # Eigenmode data
         direction = mp.NO_DIRECTION if self.kpoint_func else mp.AUTOMATIC
         ob = self.sim.get_eigenmode_coefficients(self.monitor,[self.mode],direction=direction,kpoint_func=self.kpoint_func,**self.EigenMode_kwargs)
-        self.eval = np.squeeze(ob.alpha[:,:,self.forward]) # record eigenmode coefficients for scaling 
+        self.eval = np.squeeze(ob.alpha[:,:,self.forward]) # record eigenmode coefficients for scaling
         self.cscale = ob.cscale # pull scaling factor
 
         return self.eval

python/adjoint/optimization_problem.py
@@ -31,7 +31,7 @@ class DesignRegion(object):
         f = sim.fields
         vol = sim._fit_volume_to_simulation(self.volume)
         # compute the gradient
-        mp._get_gradient(grad,fields_a,fields_f,vol,np.array(frequencies),geom_list,f) 
+        mp._get_gradient(grad,fields_a,fields_f,vol,np.array(frequencies),geom_list,f)
 
         return np.squeeze(grad).T
 
@@ -229,10 +229,10 @@ class OptimizationProblem(object):
             dJ = jacobian(self.objective_functions[objective_idx],mi)(*self.results_list) # get gradient of objective w.r.t. monitor
             if np.any(dJ):
                 self.adjoint_sources += m.place_adjoint_source(dJ) # place the appropriate adjoint sources
-        
+
         # Reset the fields
         self.sim.reset_meep()
-        
+
         # Update the sources
         self.sim.change_sources(self.adjoint_sources)
 
@@ -259,7 +259,7 @@ class OptimizationProblem(object):
     def calculate_gradient(self):
         # Iterate through all design regions and calculate gradient
         self.gradient = [[dr.get_gradient(self.sim,self.a_E[ar][dri],self.d_E[dri],self.frequencies) for dri, dr in enumerate(self.design_regions)] for ar in range(len(self.objective_functions))]
-        
+
         # Cleanup list of lists
         if len(self.gradient) == 1:
             self.gradient = self.gradient[0] # only one objective function
@@ -322,9 +322,9 @@ class OptimizationProblem(object):
             self.forward_monitors = []
             for m in self.objective_arguments:
                 self.forward_monitors.append(m.register_monitors(self.frequencies))
-            
+
             self.sim.run(until_after_sources=stop_when_dft_decayed(self.sim, self.forward_monitors, self.decay_dt, self.decay_fields, self.fcen_idx, self.decay_by, True, self.minimum_run_time))
-            
+
             # record final objective function value
             results_list = []
             for m in self.objective_arguments:
@@ -347,7 +347,7 @@ class OptimizationProblem(object):
 
             # add monitor used to track dft convergence
             self.sim.run(until_after_sources=stop_when_dft_decayed(self.sim, self.forward_monitors, self.decay_dt, self.decay_fields, self.fcen_idx, self.decay_by, True, self.minimum_run_time))
-            
+
             # record final objective function value
             results_list = []
             for m in self.objective_arguments:
@@ -410,7 +410,7 @@ def stop_when_dft_decayed(simob, mon, dt, c, fcen_idx, decay_by, yee_grid=False,
             comp = ci if yee_grid else mp.Dielectric
             ci_dims += [simob.get_array_slice_dimensions(comp,vol=m.where)[0]]
         dims.append(ci_dims)
-    
+
     # Record data in closure so that we can persitently edit
     closure = {
         'previous_fields': [[np.ones(di,dtype=np.complex128) for di in d] for d in dims],