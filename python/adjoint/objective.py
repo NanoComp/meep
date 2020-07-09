fix gradients

python/adjoint/objective.py
@@ -76,8 +76,9 @@ class EigenmodeCoefficient(ObjectiveQuantitiy):
             dV = 1/self.sim.resolution * 1/self.sim.resolution
         else:
             dV = 1/self.sim.resolution * 1/self.sim.resolution * 1/self.sim.resolution
-        da_dE = 0.5*(dV * self.cscale)
-        scale = da_dE * dJ * 1j * 2 * np.pi * self.frequencies / np.array([self.time_src.fourier_transform(f) for f in self.frequencies]) # final scale factor
+        da_dE = 0.5 * self.cscale
+        iomega = ((1.0 - np.exp(-1j * (2 * np.pi * self.frequencies) * dt)) * (1.0 / dt))
+        scale = da_dE * dV * dJ * iomega / np.array([self.time_src.fourier_transform(f) for f in self.frequencies]) # final scale factor
         if self.frequencies.size == 1:
             # Single frequency simulations. We need to drive it with a time profile.
             src = self.time_src

python/simulation.py
@@ -3167,9 +3167,9 @@ class Simulation(object):
         else:
             v = self._volume_from_kwargs(vol, center, size)
         dim_sizes = np.zeros(3, dtype=np.uintp)
-        _,_ = mp._get_array_slice_dimensions(self.fields, v, dim_sizes, False, True, component)
-        dims = [s for s in dim_sizes if s != 0]
-        return dims
+        _,_ = mp._get_array_slice_dimensions(self.fields, v, dim_sizes, False, False, component)
+        dim_sizes[dim_sizes==0] = 1
+        return dim_sizes
     
     def get_eigenmode_coefficients(self, flux, bands, eig_parity=mp.NO_PARITY, eig_vol=None,
                                    eig_resolution=0, eig_tolerance=1e-12, kpoint_func=None, direction=mp.AUTOMATIC):