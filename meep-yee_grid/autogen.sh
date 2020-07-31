f191434eafefbaea03338868b1a82005f9912421
Mo Chen <mochen@MacBook-Pro-2.local>
2020-07-30 22:36:30 -0400

    rebase

--- meep-yee_grid/autogen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# paranoia: sometimes autoconf doesn't get things right the first time
-autoreconf --verbose --install --symlink --force
-autoreconf --verbose --install --symlink --force
-autoreconf --verbose --install --symlink --force
-
-./configure --enable-maintainer-mode "$@"
--- meep-yee_grid/doc/docs/mathjaxhelper.js
+++
@@ -1,5 +0,0 @@
-MathJax.Hub.Config({
-  config: ["MMLorHTML.js"],
-  jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
-  extensions: ["MathMenu.js", "MathZoom.js"]
-});
