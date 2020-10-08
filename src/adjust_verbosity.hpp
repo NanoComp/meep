d5a1c83746a5cc8ce6cfe52cff73be395dae8dd1
Robin Dunn <robin@simpetus.com>
2020-10-08 11:07:22 -0700

    Fix compilation error when using MPB < 1.11 (#1395)

--- src/adjust_verbosity.hpp
+++ src/adjust_verbosity.hpp
@@ -27,20 +27,19 @@ namespace meep {
 class adjust_mpb_verbosity {
 public:
   adjust_mpb_verbosity() {
-    #ifdef HAVE_MPB
+    #if defined(HAVE_MPB) && (MPB_VERSION_MAJOR > 1 || (MPB_VERSION_MAJOR == 1 && MPB_VERSION_MINOR >= 11))
       old_level = mpb_verbosity;
       mpb_verbosity = verbosity - 1;
       if (mpb_verbosity < 0) mpb_verbosity = 0;
       if (mpb_verbosity > 3) mpb_verbosity = 3;
     #else
       // avoid warnings
-      (void)amount;
       (void)old_level;
     #endif
   }
 
   ~adjust_mpb_verbosity() {
-    #ifdef HAVE_MPB
+    #if defined(HAVE_MPB) && (MPB_VERSION_MAJOR > 1 || (MPB_VERSION_MAJOR == 1 && MPB_VERSION_MINOR >= 11))
       mpb_verbosity = old_level;
     #endif
   }
