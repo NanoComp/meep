d45711e580c7fe5735feb34196fd215928abedb6
Ardavan Oskooi <ardavan.oskooi@gmail.com>
2020-11-27 23:16:23 -0800

    add option to plot2D to plot contour of geometry rather than its image

--- python/simulation.py
+++ python/simulation.py
@@ -3876,6 +3876,8 @@ class Simulation(object):
             - `interpolation='spline36'`: interpolation algorithm used to upsample the pixels.
             - `cmap='binary'`: the color map of the geometry
             - `alpha=1.0`: transparency of geometry
+            - `contour=False`: if True, plot a contour of the geometry rather than its image
+            - `contour_linewidth=0.5`: width of the contour lines if `contour=True`
         * `boundary_parameters`: a `dict` of optional plotting parameters that override
           the default parameters for the boundary layers.
             - `alpha=1.0`: transparency of boundary layers
