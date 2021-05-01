198628de6c2732a0c1013f24653fc6c94c04016c
Ardavan Oskooi <ardavano@google.com>
2021-04-30 21:51:23 -0700

    unit test for material grid as default material

--- python/tests/material_grid.py
+++ python/tests/material_grid.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.ndimage import gaussian_filter
 import unittest
 
-def compute_resonant_mode(res):
+def compute_resonant_mode(res,default_mat=False):
         cell_size = mp.Vector3(1,1,0)
 
         rad = 0.301943
@@ -42,7 +42,8 @@ def compute_resonant_mode(res):
 
         sim = mp.Simulation(resolution=res,
                             cell_size=cell_size,
-                            geometry=geometry,
+                            default_material=matgrid if default_mat else mp.Medium(),
+                            geometry=geometry if not default_mat else [],
                             sources=sources,
                             k_point=k_point)
 
@@ -78,6 +79,9 @@ class TestMaterialGrid(unittest.TestCase):
         ## and is better than linear convergence because of subpixel smoothing
         self.assertLess(abs(freq_matgrid[1]-freq_ref)*(res[1]/res[0]),
                         abs(freq_matgrid[0]-freq_ref))
+
+        freq_matgrid_default_mat = compute_resonant_mode(res[0], True)
+        self.assertAlmostEqual(freq_matgrid[0], freq_matgrid_default_mat)
 
 if __name__ == '__main__':
     unittest.main()
