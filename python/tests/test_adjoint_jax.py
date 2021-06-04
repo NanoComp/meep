Fix backward gradients in EigenmodeCoefficient and update JAX adjoint test to check gradients in both forward and backward directions
Flip sign of `amp` based on group velocity direction

Original fix

python/tests/test_adjoint_jax.py
@@ -57,7 +57,15 @@ def build_straight_wg_simulation(
       eig_kpoint=mp.Vector3(1, 0, 0),
       size=mp.Vector3(0, wg_width + 2 * wg_padding, 0),
       center=[-sx / 2 + pml_width + source_to_pml, 0, 0],
-    )
+    ),
+    mp.EigenModeSource(
+      mp.GaussianSource(frequency=fmean, fwidth=fmean * gaussian_rel_width),
+      eig_band=1,
+      direction=mp.NO_DIRECTION,
+      eig_kpoint=mp.Vector3(-1, 0, 0),
+      size=mp.Vector3(0, wg_width + 2 * wg_padding, 0),
+      center=[sx / 2 - pml_width - source_to_pml, 0, 0],
+    ),
   ]
 
   nx = int(design_region_resolution * design_region_shape[0])
@@ -118,7 +126,7 @@ def build_straight_wg_simulation(
       mp.Volume(center=center, size=monitor_size),
       mode=1,
       forward=True,
-      decimation_factor=1) for center in monitor_centers
+      decimation_factor=1) for center in monitor_centers for forward in [True, False]
   ]
   return simulation, sources, monitors, design_regions, frequencies
 
@@ -170,11 +178,14 @@ class UtilsTest(unittest.TestCase):
 class WrapperTest(ApproxComparisonTestCase):
 
   @parameterized.parameterized.expand([
-    ('1500_1550bw_01relative_gaussian', onp.linspace(1 / 1.50, 1 / 1.55, 3).tolist(), 0.1, 1.0),
-    ('1550_1600bw_02relative_gaussian', onp.linspace(1 / 1.55, 1 / 1.60, 3).tolist(), 0.2, 1.0),
-    ('1500_1600bw_03relative_gaussian', onp.linspace(1 / 1.50, 1 / 1.60, 4).tolist(), 0.3, 1.0),
+    ('1500_1550bw_01relative_gaussian_port1', onp.linspace(1 / 1.50, 1 / 1.55, 3).tolist(), 0.1, 1.0, 0),
+    ('1550_1600bw_02relative_gaussian_port1', onp.linspace(1 / 1.55, 1 / 1.60, 3).tolist(), 0.2, 1.0, 0),
+    ('1500_1600bw_03relative_gaussian_port1', onp.linspace(1 / 1.50, 1 / 1.60, 4).tolist(), 0.3, 1.0, 0),
+    ('1500_1550bw_01relative_gaussian_port2', onp.linspace(1 / 1.50, 1 / 1.55, 3).tolist(), 0.1, 1.0, 1),
+    ('1550_1600bw_02relative_gaussian_port2', onp.linspace(1 / 1.55, 1 / 1.60, 3).tolist(), 0.2, 1.0, 1),
+    ('1500_1600bw_03relative_gaussian_port2', onp.linspace(1 / 1.50, 1 / 1.60, 4).tolist(), 0.3, 1.0, 1),
   ])
-  def test_wrapper_gradients(self, _, frequencies, gaussian_rel_width, design_variable_fill_value):
+  def test_wrapper_gradients(self, _, frequencies, gaussian_rel_width, design_variable_fill_value, excite_port_idx):
     """Tests gradient from the JAX-Meep wrapper against finite differences."""
     (
       simulation,
@@ -184,31 +195,29 @@ class WrapperTest(ApproxComparisonTestCase):
       frequencies,
     ) = build_straight_wg_simulation(frequencies=frequencies, gaussian_rel_width=gaussian_rel_width)
 
-    wrapped_meep = mpa.MeepJaxWrapper(
-      simulation,
-      sources,
-      monitors,
-      design_regions,
-      frequencies,
-      measurement_interval=50.0,
-      dft_field_components=(mp.Ez,),
-      dft_threshold=1e-6,
-      minimum_run_time=0,
-      maximum_run_time=onp.inf,
-      until_after_sources=True
-    )
-
     design_shape = tuple(int(i) for i in design_regions[0].design_parameters.grid_size)[:2]
     x = onp.ones(design_shape) * design_variable_fill_value
 
     # Define a loss function
-    def loss_fn(x):
+    def loss_fn(x, excite_port_idx=0):
+      wrapped_meep = mpa.MeepJaxWrapper(
+        simulation,
+        [sources[excite_port_idx]],
+        monitors,
+        design_regions,
+        frequencies,
+      )
       monitor_values = wrapped_meep([x])
-      t = monitor_values[1, :] / monitor_values[0, :]
+      s1p, s1m, s2m, s2p = monitor_values
+      if excite_port_idx == 0:
+        t = s2m / s1p
+      else:
+        t = s1m / s2p
       # Mean transmission vs wavelength
-      return jnp.mean(jnp.square(jnp.abs(t)))
+      t_mean = jnp.mean(jnp.square(jnp.abs(t)))
+      return t_mean
 
-    value, adjoint_grad = jax.value_and_grad(loss_fn)(x)
+    value, adjoint_grad = jax.value_and_grad(loss_fn)(x, excite_port_idx=excite_port_idx)
 
     projection = []
     fd_projection = []
@@ -225,7 +234,7 @@ class WrapperTest(ApproxComparisonTestCase):
       x_perturbed = x + random_perturbation_vector
 
       # Calculate T(p + dp)
-      value_perturbed = loss_fn(x_perturbed)
+      value_perturbed = loss_fn(x_perturbed, excite_port_idx=excite_port_idx)
 
       projection.append(
         onp.dot(