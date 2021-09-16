4e1b67cbfdd50f0b281cac9a2e957aab8ccceb4c
Ben Bartlett <benjamincbartlett@gmail.com>
2021-09-16 15:31:42 -0700

    Implementation of chunk balancer feature
    CONTEXT: This feature uses empirical data about the timings of each MPI process to adjust the chunk sizes to achieve optimal load balancing for repeated simulations, such as iterations in an optimization run. Processes which are consistently slow and overworked will have their chunk sizes reduced for the next iteration of an optimization run, and underworked processes will handle larger chunks. This approach has the advantage that it implicitly incorporates variable load between different machines running the MPI processes.
    
    SCOPE:
    - Added abstract class for chunk balancer
    - Added DefaultChunkBalancer implementation, which adjusts chunk sizes according to per-process working time (ignoring all-all comms) while maintaining previous split directions
    - Wrote unit tests for DefaultChunkBalancer to check for improvement in load-balancing, convergence to a load-balanced state, and that split_pos values are adjusted correctly for each iteration.
    - Wrote unit tests for the MockSimulation class used by the DefaultChunkBalancerTests
    - Added a binary_partition_utils.py library which includes lots of tree traversal algorithms which are useful to have for the chunk balancer
    - Wrote unit tests for binary_partition_utils.py

---
+++ python/tests/test_timing_measurements.py
@@ -0,0 +1,36 @@
+import time
+import unittest
+
+import meep as mp
+from meep import timing_measurements as timing
+
+
+class TimingTest(unittest.TestCase):
+
+    def test_timing_measurements(self):
+        """Tests that timing measurements have expected names and can be updated."""
+        sim = mp.Simulation(
+            cell_size=mp.Vector3(2, 2, 2),
+            resolution=20,
+        )
+        time_start = time.time()
+        sim.run(until=5)
+        timing_measurements = timing.MeepTimingMeasurements.new_from_simulation(
+            sim=sim,
+            elapsed_time=time.time() - time_start,
+            time_per_step=[0.1],
+        )
+
+        # Check for expected names after updating
+        self.assertSetEqual(
+            set(timing_measurements.measurement_names),
+            set(timing.TIMING_MEASUREMENT_IDS.keys()),
+        )
+        self.assertGreater(timing_measurements.elapsed_time, 0)
+        self.assertGreater(timing_measurements.num_time_steps, 0)
+        self.assertGreaterEqual(timing_measurements.comm_efficiency, 0)
+        self.assertGreaterEqual(timing_measurements.comm_efficiency_one_to_one, 0)
+        self.assertGreaterEqual(timing_measurements.comm_efficiency_all_to_all, 0)
+
+if __name__ == '__main__':
+    unittest.main()
