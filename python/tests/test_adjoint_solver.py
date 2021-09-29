fix off-by-one bug in dimensions of fixed-resolution MaterialGrid for adjoint-solver functions (#1769)
* fix off by one bug in MaterialGrid dimensions of adjoint solver functions

* adjust tols

* formatting fixes

* add 1 after dividing by 2

* change design_shape to design_region_size

python/tests/test_adjoint_solver.py
@@ -12,7 +12,7 @@ from utils import ApproxComparisonTestCase
 
 MonitorObject = Enum('MonitorObject', 'EIGENMODE DFT')
 
-resolution = 25
+resolution = 30
 
 silicon = mp.Medium(epsilon=12)
 
@@ -26,8 +26,8 @@ eig_parity = mp.EVEN_Y + mp.ODD_Z
 
 design_region_size = mp.Vector3(1.5,1.5)
 design_region_resolution = int(2*resolution)
-Nx = int(design_region_resolution*design_region_size.x)
-Ny = int(design_region_resolution*design_region_size.y)
+Nx = int(design_region_resolution*design_region_size.x) + 1
+Ny = int(design_region_resolution*design_region_size.y) + 1
 
 ## ensure reproducible results
 np.random.seed(9861548)
@@ -213,7 +213,7 @@ class TestAdjointSolver(ApproxComparisonTestCase):
             adj_scale = (dp[None,:]@adjsol_grad).flatten()
             fd_grad = S12_perturbed-S12_unperturbed
             print("Directional derivative -- adjoint solver: {}, FD: {}".format(adj_scale,fd_grad))
-            tol = 0.3 if mp.is_single_precision() else 0.01
+            tol = 0.04 if mp.is_single_precision() else 0.005
             self.assertClose(adj_scale,fd_grad,epsilon=tol)
 
 
@@ -231,7 +231,7 @@ class TestAdjointSolver(ApproxComparisonTestCase):
 
                     ## compare objective results
                     print("S12 -- adjoint solver: {}, traditional simulation: {}".format(adjsol_obj,S12_unperturbed))
-                    self.assertClose(adjsol_obj,S12_unperturbed,epsilon=1e-3)
+                    self.assertClose(adjsol_obj,S12_unperturbed,epsilon=1e-6)
 
                     ## compute perturbed S12
                     S12_perturbed = forward_simulation(p+dp, MonitorObject.EIGENMODE, frequencies, use_complex, k)
@@ -242,7 +242,7 @@ class TestAdjointSolver(ApproxComparisonTestCase):
                     adj_scale = (dp[None,:]@adjsol_grad).flatten()
                     fd_grad = S12_perturbed-S12_unperturbed
                     print("Directional derivative -- adjoint solver: {}, FD: {}".format(adj_scale,fd_grad))
-                    tol = 0.15 if mp.is_single_precision() else 0.03
+                    tol = 0.04 if mp.is_single_precision() else 0.01
                     self.assertClose(adj_scale,fd_grad,epsilon=tol)
 
     def test_gradient_backpropagation(self):
@@ -271,8 +271,7 @@ class TestAdjointSolver(ApproxComparisonTestCase):
 
             ## compare objective results
             print("S12 -- adjoint solver: {}, traditional simulation: {}".format(adjsol_obj,S12_unperturbed))
-            tol = 1e-2 if mp.is_single_precision() else 1e-3
-            self.assertClose(adjsol_obj,S12_unperturbed,epsilon=tol)
+            self.assertClose(adjsol_obj,S12_unperturbed,epsilon=1e-6)
 
             ## compute perturbed S12
             S12_perturbed = forward_simulation(mapping(p+dp,filter_radius,eta,beta), MonitorObject.EIGENMODE,frequencies)
@@ -282,7 +281,7 @@ class TestAdjointSolver(ApproxComparisonTestCase):
             adj_scale = (dp[None,:]@bp_adjsol_grad).flatten()
             fd_grad = S12_perturbed-S12_unperturbed
             print("Directional derivative -- adjoint solver: {}, FD: {}".format(adj_scale,fd_grad))
-            tol = 0.05 if mp.is_single_precision() else 0.03
+            tol = 0.02 if mp.is_single_precision() else 0.01
             self.assertClose(adj_scale,fd_grad,epsilon=tol)
 
 