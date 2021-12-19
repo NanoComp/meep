b8858990ba08c84a2b99d5a6959cdd68c983b8d5
Ardavan Oskooi <ardavano@google.com>
2021-12-19 05:33:03 +0000

    unit test for conductivity

---
+++ python/tests/test_conductivity.py
@@ -0,0 +1,91 @@
+import unittest
+import numpy as np
+import meep as mp
+
+class TestConductivity(unittest.TestCase):
+
+    def wvg_flux(self, res, att_coeff):
+        """
+        Computes the Poynting flux in a single-mode waveguide at two
+        locations (5 and 10 μm) downstream from the source given the
+        grid resolution res (pixels/μm) and material attenutation
+        coefficient att_coeff (dB/cm).
+        """
+
+        cell_size = mp.Vector3(14.,14.)
+
+        pml_layers = [mp.PML(thickness=2.)]
+
+        w = 1. # width of waveguide
+
+        fsrc = 0.15 # frequency (in vacuum)
+
+        # note: MPB only computes modes of lossless materials and thus when
+        #       when att_coeff > 0, the launched waveguide mode is inaccurate.
+        #       The inaccuracy increases with increasing att_coeff.
+        sources = [mp.EigenModeSource(src=mp.GaussianSource(fsrc,fwidth=0.2*fsrc),
+                                      center=mp.Vector3(-5.),
+                                      size=mp.Vector3(y=10.),
+                                      eig_parity=mp.EVEN_Y+mp.ODD_Z)]
+
+        # ref: https://en.wikipedia.org/wiki/Mathematical_descriptions_of_opacity
+        n_eff = np.sqrt(12.) + 1j * (1/fsrc) * (1e-4 * att_coeff) / (4 * np.pi)
+        eps_eff = n_eff * n_eff
+        # ref: https://meep.readthedocs.io/en/latest/Materials/#conductivity-and-complex
+        sigma_D = 2 * np.pi * fsrc * np.imag(eps_eff) / np.real(eps_eff)
+
+        geometry = [mp.Block(center=mp.Vector3(),
+                             size=mp.Vector3(mp.inf,w,mp.inf),
+                             material=mp.Medium(epsilon=np.real(eps_eff),
+                                                D_conductivity=sigma_D))]
+
+        sim = mp.Simulation(cell_size=cell_size,
+                            resolution=res,
+                            boundary_layers=pml_layers,
+                            sources=sources,
+                            geometry=geometry,
+                            symmetries=[mp.Mirror(mp.Y)])
+
+        tran1 = sim.add_flux(fsrc,
+                             0,
+                             1,
+                             mp.FluxRegion(center=mp.Vector3(x=0.), size=mp.Vector3(y=10.)))
+        tran2 = sim.add_flux(fsrc,
+                             0,
+                             1,
+                             mp.FluxRegion(center=mp.Vector3(x=5.), size=mp.Vector3(y=10.)))
+
+        sim.run(until_after_sources=20)
+
+        return mp.get_fluxes(tran1)[0], mp.get_fluxes(tran2)[0]
+
+
+    def test_conductivity(self):
+        res = 25. # pixels/μm
+
+        # compute the incident flux for a lossless waveguide
+        incident_flux1, incident_flux2 = self.wvg_flux(res, 0.)
+        self.assertAlmostEqual(incident_flux1/incident_flux2, 1.0, places=2)
+        print("incident_flux:, {} (measured), 1.0 (expected)".format(incident_flux2/incident_flux1))
+
+        # compute the flux for a lossy waveguide
+        att_coeff = 37. # dB/cm
+        attenuated_flux1, attenuated_flux2 = self.wvg_flux(res, att_coeff)
+
+        L1 = 5.
+        expected_att1 = np.exp(-att_coeff * 1e-4 * L1)
+        self.assertAlmostEqual(attenuated_flux1/incident_flux2, expected_att1, places=2)
+        print("flux:, {}, {:.6f} (measured), {:.6f} (expected)".format(L1,
+                                                                       attenuated_flux1/incident_flux2,
+                                                                       expected_att1))
+
+        L2 = 10.
+        expected_att2 = np.exp(-att_coeff * 1e-4 * L2)
+        self.assertAlmostEqual(attenuated_flux2/incident_flux2, expected_att2, places=2)
+        print("flux:, {}, {:.6f} (measured), {:.6f} (expected)".format(L2,
+                                                                       attenuated_flux2/incident_flux2,
+                                                                       expected_att2))
+
+
+if __name__ == '__main__':
+    unittest.main()
