ee77c25ee896f045cf0e92190526934c10f5b82f
Ardavan Oskooi <ardavano@google.com>
2022-05-18 15:49:16 -0700

    tutorial and unit test for LDOS of planar cavity with lossless metallic walls

--- python/tests/test_ldos.py
+++ python/tests/test_ldos.py
@@ -1,52 +1,100 @@
 import unittest
-
+import numpy as np
 import meep as mp
 
+# Computes the Purcell enhancement factor of a horizontal dipole in a 3D
+# homogeneous dielectric cavity with lossless metallic walls on two sides.
+# The simulated result is validated using analytic theory from
+# I. Abram et al., IEEE J. Quantum Electronics, Vol. 34, pp. 71-76 (1998).
 
 class TestLDOS(unittest.TestCase):
 
-    def setUp(self):
-        resolution = 20
+    @classmethod
+    def setUp(cls):
+        cls.resolution = 20  # pixels/μm
+        cls.dpml = 0.5       # thickness of PML
+        cls.L = 6.0          # length of non-PML region
+        cls.n = 2.4          # refractive index of surrounding medium
+        cls.wvl = 1.0        # wavelength (in vacuum)
 
-        cell = mp.Vector3(10, 10, 0)
+        cls.fcen = 1/cls.wvl
+        cls.sources = [mp.Source(src=mp.GaussianSource(cls.fcen,fwidth=0.2*cls.fcen),
+                                 component=mp.Ex,
+                                 center=mp.Vector3())]
 
-        pml_layers = mp.PML(1.0)
+        cls.symmetries = [mp.Mirror(direction=mp.X,phase=-1),
+                          mp.Mirror(direction=mp.Y),
+                          mp.Mirror(direction=mp.Z)]
 
-        self.fcen = 1.0
-        df = 1.0
 
-        sources = mp.Source(src=mp.GaussianSource(self.fcen, fwidth=df), center=mp.Vector3(),
-                            component=mp.Ez)
+    def bulk_ldos(self):
+        s = self.L+2*self.dpml
+        cell_size = mp.Vector3(s,s,s)
 
-        symmetries = [mp.Mirror(mp.X), mp.Mirror(mp.Y)]
+        pml_layers = [mp.PML(self.dpml)]
 
-        self.sim = mp.Simulation(resolution=resolution,
-                                 cell_size=cell,
-                                 boundary_layers=[pml_layers],
-                                 sources=[sources],
-                                 symmetries=symmetries)
+        sim = mp.Simulation(resolution=self.resolution,
+                            cell_size=cell_size,
+                            boundary_layers=pml_layers,
+                            sources=self.sources,
+                            symmetries=self.symmetries,
+                            default_material=mp.Medium(index=self.n))
+
+        sim.run(mp.dft_ldos(self.fcen,0,1),
+                until_after_sources=mp.stop_when_fields_decayed(20,
+                                                                mp.Ex,
+                                                                mp.Vector3(),
+                                                                1e-6))
+
+        return sim.ldos_data[0]
+
+
+    def cavity_ldos(self,sz):
+        sxy = self.L+2*self.dpml
+        cell_size = mp.Vector3(sxy,sxy,sz)
+
+        boundary_layers = [mp.PML(self.dpml,direction=mp.X),
+                           mp.PML(self.dpml,direction=mp.Y)]
+
+        sim = mp.Simulation(resolution=self.resolution,
+                            cell_size=cell_size,
+                            boundary_layers=boundary_layers,
+                            sources=self.sources,
+                            symmetries=self.symmetries,
+                            default_material=mp.Medium(index=self.n))
+
+        sim.run(mp.dft_ldos(ldos=mp.Ldos(self.fcen,0,1)),
+                until_after_sources=mp.stop_when_fields_decayed(20,
+                                                                mp.Ex,
+                                                                mp.Vector3(),
+                                                                1e-6))
+
+        return sim.ldos_data[0]
+
 
     def test_ldos(self):
-        self.sim.run(
-            mp.dft_ldos(self.fcen, 0, 1),
-            until_after_sources=mp.stop_when_fields_decayed(50, mp.Ez, mp.Vector3(), 1e-6)
-        )
+        ldos_bulk = self.bulk_ldos()
+        print("ldos_bulk:, {:.6f}".format(ldos_bulk))
 
-        self.assertAlmostEqual(self.sim.ldos_data[0], 1.011459560620368)
+        cavity_thickness = 0.75
+        gap = cavity_thickness*self.wvl/self.n
 
-    def test_ldos_user_object(self):
-        ldos = mp.Ldos(self.fcen, 0, 1)
-        self.sim.run(
-            mp.dft_ldos(ldos=ldos),
-            until_after_sources=mp.stop_when_fields_decayed(50, mp.Ez, mp.Vector3(), 1e-6)
-        )
+        ldos_cavity = self.cavity_ldos(gap)
 
-        self.assertAlmostEqual(self.sim.ldos_data[0], 1.011459560620368)
-        self.assertEqual(len(mp.get_ldos_freqs(ldos)), 1)
+        # Purcell enhancement factor (relative to bulk medium)
+        pe_meep = ldos_cavity/ldos_bulk
 
-    def test_invalid_dft_ldos(self):
-        with self.assertRaises(TypeError):
-            self.sim.run(mp.dft_ldos(mp.Ldos(self.fcen, 0, 1)), until=200)
+        # equation 7 of reference
+        pe_theory = (3*np.fix(cavity_thickness+0.5)/(4*cavity_thickness) +
+                     (4*np.power(np.fix(cavity_thickness+0.5),3) -
+                      np.fix(cavity_thickness+0.5))/(16*np.power(cavity_thickness,3)))
+
+        rel_err = abs(pe_meep-pe_theory)/pe_theory
+
+        print("ldos:, {:.6f} (Meep), {:.6f} (theory), "
+              "{:.6f} (error)".format(pe_meep,pe_theory,rel_err))
+
+        self.assertAlmostEqual(pe_meep, pe_theory, delta=0.1)
 
 
 if __name__ == '__main__':
