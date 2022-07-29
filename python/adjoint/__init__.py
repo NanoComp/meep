83dc114c4047d3bc9b5ceec2d4d31a8973a60997
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-07-29 11:51:09 -0700

    remove isort from pre-commit

--- python/adjoint/__init__.py
+++ python/adjoint/__init__.py
@@ -3,14 +3,20 @@ Adjoint-based sensitivity-analysis module for pymeep.
 Authors: Homer Reid <homer@homerreid.com>, Alec Hammond <alec.hammond@gatech.edu>, Ian Williamson <iwill@google.com>
 """
 
-from . import utils
-from .basis import BilinearInterpolationBasis
-from .connectivity import *
-from .filter_source import FilteredSource
-from .filters import *
 from .objective import *
-from .optimization_problem import OptimizationProblem
+
+from . import utils
 from .utils import DesignRegion
+
+from .basis import BilinearInterpolationBasis
+
+from .optimization_problem import OptimizationProblem
+
+from .filter_source import FilteredSource
+
+from .filters import *
+
+from .connectivity import *
 
 try:
     from .wrapper import MeepJaxWrapper
