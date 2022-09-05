59b23e50b7b3f231f4c52c054e2252b69f78f024
Yaraslau Tamashevich <yaraslau.tamashevich@gmail.com>
2022-09-05 11:06:57 +0300

    Build meep script update
    * refactor of dependency installation
    * refactor of compilation part
    * added support for ubunutu 22.04 and 21.10
    * added few flags to give more options during compilation

--- contrib/build-meep.sh
+++ contrib/build-meep.sh
@@ -20,7 +20,8 @@ EOF
 
 installdeps=true
 with_guile=true
-with_parallel=true
+with_openmp=true
+with_mpi=true
 
 while [ ! -z "$1" ]; do
     case "$1" in
@@ -37,11 +38,15 @@ while [ ! -z "$1" ]; do
         -n)         # do not check for distribution dependencies
             installdeps=false
             ;;
-        -g)        # build without guile
+        --without-guile)        # build without guile
             with_guile=false
             ;;
-        -se)        # build serial version
-            with_parallel=false
+        --without-parallel)        # build serial version
+            with_openmp=false
+            with_mpi=false
+            ;;
+        --without-mpi)        # build without mpi
+            with_mpi=false
             ;;
         *)
             echo "'$1' ?"
@@ -69,7 +74,7 @@ Please ensure the following final paths fit your needs:
 
 Press return to continue
 EOF
-#read junk
+read junk
 
 if ! lsb_release; then
     echo "Minimum requirements:"
@@ -115,139 +120,167 @@ configure()
 
 getlibctl()
 {
-    if $with_guile; then
-        gitclone https://github.com/NanoComp/libctl.git
+    if [ -d libctl ]; then
+        if $with_guile; then
+            (cd libctl; git pull; cd ./..)
+        fi
     else
-        wget https://github.com/NanoComp/libctl/releases/download/v4.5.1/libctl-4.5.1.tar.gz
-        tar -xzf libctl-4.5.1.tar.gz
-        mv libctl-4.5.1 libctl
-        rm libctl-4.5.1.tar.gz
-        echo $PWD
+
+        if $with_guile; then
+            gitclone https://github.com/NanoComp/libctl.git
+        else
+            wget https://github.com/NanoComp/libctl/releases/download/v4.5.1/libctl-4.5.1.tar.gz
+            tar -xzf libctl-4.5.1.tar.gz
+            mv libctl-4.5.1 libctl
+            rm libctl-4.5.1.tar.gz
+            echo $PWD
+        fi
     fi
 }
 
-if $installdeps; then
 
-    ubuntu=false
-    centos=false
+fetch_compile_install()
+{
+    repo=$1
+    name=${repo%%.*}
+    flags="${@:2}"
+    cd $SRCDIR
+    gitclone $repo
+    cd $name
+    autogensh $flags
+    make -j && $SUDO make install
+}
 
+ubuntudeps()
+{
     distrib=$(lsb_release -r -s)
     case "$distrib" in
-        18.04) # ubuntu 18.04 bionic
+        22.04|21.10)
+            libguile=guile-2.2-dev
             libpng=libpng-dev
             libpython=libpython3-dev
-            ubuntu=true
             ;;
         16.04) # ubuntu 16.04 xenial
             libpng=libpng16-dev
             libpython=libpython3.5-dev
-            ubuntu=true
+            libguile=guile-2.0-dev
             ;;
-        7.*) # CentOS 7.x
-            centos=true
+        *) # 20.04 | 18.04
+            libguile=guile-2.0-dev
+            libpng=libpng-dev
+            libpython=libpython3-dev
             ;;
-        *)
-            echo "unsupported distribution '$(lsb_release -a)', edit and fix!"
-            false
-            ;;
-    esac
+        esac
 
-    if $ubuntu; then
+    sudo apt-get update
 
-        sudo apt-get update
+    sudo apt-get -y install     \
+        build-essential         \
+        gfortran                \
+        libblas-dev             \
+        liblapack-dev           \
+        libgmp-dev              \
+        swig                    \
+        libgsl-dev              \
+        autoconf                \
+        pkg-config              \
+        $libpng                 \
+        git                     \
+        guile-2.0-dev           \
+        libfftw3-dev            \
+        libhdf5-openmpi-dev     \
+        hdf5-tools              \
+        $libpython              \
+        python3-numpy           \
+        python3-scipy           \
+        python3-pip             \
+        ffmpeg                  \
 
-        sudo apt-get -y install     \
-            build-essential         \
-            gfortran                \
-            libblas-dev             \
-            liblapack-dev           \
-            libgmp-dev              \
-            swig                    \
-            libgsl-dev              \
-            autoconf                \
-            pkg-config              \
-            $libpng                 \
-            git                     \
-            guile-2.0-dev           \
-            libfftw3-dev            \
-            libhdf5-openmpi-dev     \
-            hdf5-tools              \
-            $libpython              \
-            python3-numpy           \
-            python3-scipy           \
-            python3-pip             \
-            ffmpeg                  \
+    [ "$distrib" = 16.04 ] && sudo -H pip3 install --upgrade pip
+    sudo -H pip3 install --no-cache-dir mpi4py
+    export HDF5_MPI="ON"
+    sudo -H pip3 install --no-binary=h5py h5py
+    sudo -H pip3 install matplotlib>3.0.0
 
-        [ "$distrib" = 16.04 ] && sudo -H pip3 install --upgrade pip
-        sudo -H pip3 install --no-cache-dir mpi4py
-        export HDF5_MPI="ON"
-        sudo -H pip3 install --no-binary=h5py h5py
-        sudo -H pip3 install matplotlib>3.0.0
+    RPATH_FLAGS="-Wl,-rpath,${DESTDIR}/lib:/usr/lib/x86_64-linux-gnu/hdf5/openmpi"
+    LDFLAGS="-L${DESTDIR}/lib -L/usr/lib/x86_64-linux-gnu/hdf5/openmpi ${RPATH_FLAGS}"
+    CFLAGS="-I${DESTDIR}/include -I/usr/include/hdf5/openmpi"
+}
 
-        RPATH_FLAGS="-Wl,-rpath,${DESTDIR}/lib:/usr/lib/x86_64-linux-gnu/hdf5/openmpi"
-        LDFLAGS="-L${DESTDIR}/lib -L/usr/lib/x86_64-linux-gnu/hdf5/openmpi ${RPATH_FLAGS}"
-        CFLAGS="-I${DESTDIR}/include -I/usr/include/hdf5/openmpi"
+centosdeps()
+{
+    sudo yum -y --enablerepo=extras install epel-release
 
-    fi
+    sudo yum -y install   \
+        bison             \
+        byacc             \
+        cscope            \
+        ctags             \
+        cvs               \
+        diffstat          \
+        oxygen            \
+        flex              \
+        gcc               \
+        gcc-c++           \
+        gcc-gfortran      \
+        gettext           \
+        git               \
+        indent            \
+        intltool          \
+        libtool           \
+        patch             \
+        patchutils        \
+        rcs               \
+        redhat-rpm-config \
+        rpm-build         \
+        subversion        \
+        systemtap         \
+        wget
 
-    if $centos; then
+    sudo yum -y install    \
+        openblas-devel     \
+        fftw3-devel        \
+        libpng-devel       \
+        gsl-devel          \
+        gmp-devel          \
+        pcre-devel         \
+        libtool-ltdl-devel \
+        libunistring-devel \
+        libffi-devel       \
+        gc-devel           \
+        zlib-devel         \
+        openssl-devel      \
+        sqlite-devel       \
+        bzip2-devel        \
+        ffmpeg
 
-        sudo yum -y --enablerepo=extras install epel-release
-
-        sudo yum -y install   \
-            bison             \
-            byacc             \
-            cscope            \
-            ctags             \
-            cvs               \
-            diffstat          \
-            oxygen            \
-            flex              \
-            gcc               \
-            gcc-c++           \
-            gcc-gfortran      \
-            gettext           \
-            git               \
-            indent            \
-            intltool          \
-            libtool           \
-            patch             \
-            patchutils        \
-            rcs               \
-            redhat-rpm-config \
-            rpm-build         \
-            subversion        \
-            systemtap         \
-            wget
-
-        sudo yum -y install    \
-            openblas-devel     \
-            fftw3-devel        \
-            libpng-devel       \
-            gsl-devel          \
-            gmp-devel          \
-            pcre-devel         \
-            libtool-ltdl-devel \
-            libunistring-devel \
-            libffi-devel       \
-            gc-devel           \
-            zlib-devel         \
-            openssl-devel      \
-            sqlite-devel       \
-            bzip2-devel        \
-            ffmpeg
-
-        sudo yum -y install    \
-            openmpi-devel      \
-            hdf5-openmpi-devel \
-            guile-devel        \
-            swig
+    sudo yum -y install    \
+        openmpi-devel      \
+        hdf5-openmpi-devel \
+        guile-devel        \
+        swig
 
     export PATH=${PATH}:/usr/lib64/openmpi/bin
     RPATH_FLAGS="-Wl,-rpath,${DESTDIR}/lib:/usr/lib64/openmpi/lib"
     LDFLAGS="-L${DESTDIR}/lib -L/usr/lib64/openmpi/lib ${RPATH_FLAGS}"
     CFLAGS="-I${DESTDIR}/include -I/usr/include/openmpi-x86_64/"
-    fi
+}
+
+if $installdeps; then
+
+    distrib=$(grep ^ID= /etc/os-release | cut -d= -f2)
+    case "$distrib" in
+        ubuntu)
+            ubuntudeps
+            ;;
+        centos)
+            centosdeps
+            ;;
+        *)
+            echo "OS not supported."
+            ;;
+    esac
+
 else
     RPATH_FLAGS="-Wl,-rpath,-L${DESTDIR}/lib"
     LDFLAGS="-L${DESTDIR}/lib"
@@ -269,21 +302,27 @@ else
     guile_flag_meep="--without-scheme"
 fi
 
-if $with_parallel; then
-    parallel_flags="--with-mpi --with-openmp"
+if $with_openmp; then
+    parallel_flags="--with-openmp"
 else
     parallel_flags=""
+fi
+
+if $with_mpi; then
+    parallel_flags="${parallel_flags} --with-mpi"
+else
+    parallel_flags="${parallel_flags}"
 fi
 
 mkdir -p $SRCDIR
 
 
-cd $SRCDIR
-gitclone https://github.com/NanoComp/harminv.git
-cd harminv/
-autogensh ${CONFIGURE_harminv}
-make -j && $SUDO make install
+#harminv
+fetch_compile_install \
+    https://github.com/NanoComp/harminv.git \
+    ${CONFIGURE_harminv}
 
+#libctl
 cd $SRCDIR
 getlibctl
 cd libctl
@@ -295,28 +334,24 @@ fi
 make -j && $SUDO make install
 
 
-cd $SRCDIR
-gitclone https://github.com/NanoComp/h5utils.git
-cd h5utils/
-autogensh ${if_guile_flag} ${CONFIGURE_h5utils}
-make -j && $SUDO make install
+#h5utils
+fetch_compile_install \
+    https://github.com/NanoComp/h5utils.git \
+    ${if_guile_flag} ${CONFIGURE_h5utils} \
 
-cd $SRCDIR
-gitclone https://github.com/NanoComp/mpb.git
-cd mpb/
-autogensh --with-hermitian-eps --with-libctl=${DESTDIR}/share/libctl ${guile_flag_mpb} ${CONFIGURE_mpb}
-make -j && $SUDO make install
+#mpb
+fetch_compile_install \
+    https://github.com/NanoComp/mpb.git\
+     --with-hermitian-eps --with-libctl=${DESTDIR}/share/libctl ${guile_flag_mpb} ${CONFIGURE_mpb}
 
-cd $SRCDIR
-gitclone https://github.com/HomerReid/libGDSII.git
-cd libGDSII/
-autogensh ${CONFIGURE_libGDSII}
-make -j && $SUDO make install
+#libGDSII
+fetch_compile_install \
+    https://github.com/HomerReid/libGDSII.git\
+    ${CONFIGURE_libGDSII}
 
-cd $SRCDIR
-gitclone https://github.com/NanoComp/meep.git
-cd meep/
-autogensh ${parallel_flags} PYTHON=python3 --with-libctl=${DESTDIR}/share/libctl ${guile_flag_meep} ${CONFIGURE_meep}
-make -j && $SUDO make install
+#meep
+fetch_compile_install \
+    https://github.com/NanoComp/meep.git\
+    autogensh ${parallel_flags} PYTHON=python3 --with-libctl=${DESTDIR}/share/libctl ${guile_flag_meep} ${CONFIGURE_meep}
 
 # all done
