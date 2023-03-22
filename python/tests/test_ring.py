eff6c2d4ef4cc89b70c71a87d746b9f0ad748a09
Arjun-Khurana <arjunkh99@gmail.com>
2023-03-22 14:37:37 -0400

    changed pade test to specify point volume

--- python/tests/test_ring.py
+++ python/tests/test_ring.py
@@ -46,7 +46,9 @@ class TestRing(unittest.TestCase):
 
         self.sim.use_output_directory(self.temp_dir)
         self.h = mp.Harminv(mp.Ez, mp.Vector3(r + 0.1), fcen, df)
-        self.p = mp.PadeDFT(mp.Ez, mp.Vector3(r + 0.1), sampling_interval=4)
+        self.p = mp.PadeDFT(
+            c=mp.Ez, center=mp.Vector3(r + 0.1), size=mp.Vector3(), sampling_interval=4
+        )
 
     def test_harminv(self):
         self.init()
