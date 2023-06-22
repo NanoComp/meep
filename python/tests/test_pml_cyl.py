2f9874bed9ff5e109e9cff9f8bad534446ccd053
Ardavan Oskooi <ardavano@google.com>
2023-06-22 20:12:50 +0000

    remove hack for m = -1 test case

--- python/tests/test_pml_cyl.py
+++ python/tests/test_pml_cyl.py
@@ -43,18 +43,9 @@ class TestPMLCylindrical(unittest.TestCase):
             mp.PML(self.dpml_z, direction=mp.Z),
         ]
 
-        # A hack to ensure the test passes due to a bug
-        # in the z-PML at r=0 for |m| = 1. See #2182.
-        if abs(m) == 1:
-            fwidth = 0.02 * self.fcen
-            cutoff = 10.0
-        else:
-            fwidth = 0.1 * self.fcen
-            cutoff = 5.0
-
         sources = [
             mp.Source(
-                src=mp.GaussianSource(self.fcen, fwidth=fwidth, cutoff=cutoff),
+                src=mp.GaussianSource(self.fcen, fwidth=0.1 * self.fcen),
                 center=mp.Vector3(rpos, 0, 0),
                 component=mp.Er,
             ),
