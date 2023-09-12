6c5c8dbed9fb421d9e869eb700b24c710adf547c
Ardavan Oskooi <ardavano@google.com>
2023-09-11 22:40:48 -0700

    fix off-by-one error in design-region dimensions of mode-converter example

--- python/examples/adjoint_optimization/mode_converter.py
+++ python/examples/adjoint_optimization/mode_converter.py
@@ -66,8 +66,8 @@ Si = mp.Medium(index=nSi)
 
 design_region_size = mp.Vector3(dx, dy, 0)
 design_region_resolution = int(2 * resolution)
-Nx = int(design_region_size.x * design_region_resolution)
-Ny = int(design_region_size.y * design_region_resolution)
+Nx = int(design_region_size.x * design_region_resolution) + 1
+Ny = int(design_region_size.y * design_region_resolution) + 1
 
 # impose a bit "mask" of thickness equal to the filter radius
 # around the edges of the design region in order to prevent
