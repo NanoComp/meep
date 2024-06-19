85a7ea9185093da56fe2bf0fbb5f3fa81f8287b7
Ardavan Oskooi <ardavan.oskooi@gmail.com>
2024-06-18 22:55:47 -0700

    replace smoothing of the levelset using a signed-distance function with downsampling from a high-resolution grid

--- python/examples/adjoint_optimization/binary_grating_levelset.py
+++ python/examples/adjoint_optimization/binary_grating_levelset.py
@@ -1,146 +1,198 @@
-"""Validates the adjoint gradient of a level set.
+"""Computes the adjoint gradient of a level set.
 
-This is a 2D test which uses subpixel smoothing of the MaterialGrid for
-computing the gradient of the diffraction efficiency of the first transmitted
-order of a 1D grating. The adjoint gradient is validated using the brute-force
-finite difference via the directional derivative. The grating structure is
+This is a 2D example for computing the gradient of the diffraction efficiency
+of the first transmitted order of a 1D grating with respect to the grating
+height. The adjoint gradient is validated using the brute-force finite
+difference via the directional derivative. The grating structure is
 represented as a level set.
 """
 
 from enum import Enum
-from typing import Tuple
+from typing import Callable, Tuple
 
 from autograd.extend import primitive, defvjp
-from autograd import numpy as anp
+from autograd import numpy as npa
 from autograd import tensor_jacobian_product
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.pyplot as plt
 import meep as mp
 import meep.adjoint as mpa
-from scipy.interpolate import interp1d
-import skfmm
+import numpy as np
+from scipy.interpolate import RegularGridInterpolator
 
 
-RESOLUTION_UM = 200
+RESOLUTION_UM = 100
 WAVELENGTH_UM = 0.53
-GRATING_PERIOD_UM = 1.2
-PADDING_UM = 1.5
+GRATING_PERIOD_UM = 1.28
+GRATING_HEIGHT_UM = 0.52
+GRATING_DUTY_CYCLE = 0.65
+GRATING_HEIGHT_PERTURBATION_UM = 2.0 / RESOLUTION_UM
+DIFFRACTION_ORDER = 1
+SUBSTRATE_UM = 1.5
+PML_UM = 1.0
+AIR_UM = 1.0
+N_GLASS = 1.5
+DESIGN_REGION_RESOLUTION_UM = 10 * RESOLUTION_UM
 
 Polarization = Enum("Polarization", "S P")
 
+design_region_size = mp.Vector3(
+    GRATING_HEIGHT_UM + GRATING_HEIGHT_PERTURBATION_UM, GRATING_PERIOD_UM, 0
+)
+nx_design_grid = int(design_region_size.x * DESIGN_REGION_RESOLUTION_UM) + 1
+ny_design_grid = int(design_region_size.y * DESIGN_REGION_RESOLUTION_UM) + 1
+nx_sim_grid = int(design_region_size.x * RESOLUTION_UM) + 1
+ny_sim_grid = int(design_region_size.y * RESOLUTION_UM) + 1
 
-def design_region_to_meshgrid(
-    design_region_size: mp.Vector3, nx: int, ny: int
-) -> Tuple[anp.ndarray, anp.ndarray]:
-    """Returns the 2D coordinates of the meshgrid for the design region."""
+DEBUG_OUTPUT = False
 
-    xcoord = anp.linspace(-0.5 * design_region_size.x, +0.5 * design_region_size.x, nx)
-    ycoord = anp.linspace(-0.5 * design_region_size.y, +0.5 * design_region_size.y, ny)
-    xv, yv = anp.meshgrid(xcoord, ycoord, indexing="ij")
+
+def design_region_to_meshgrid(nx: int, ny: int) -> Tuple[np.ndarray, np.ndarray]:
+    """Returns the 2D coordinates of the meshgrid for the design region.
+
+    Args:
+      nx, ny: the number of grid points in the x and y directions, respectively.
+
+    Returns:
+      The coordinates of the x and y grid points (2D arrays) as a 2-tuple.
+    """
+
+    xcoord = np.linspace(-0.5 * design_region_size.x, +0.5 * design_region_size.x, nx)
+    ycoord = np.linspace(-0.5 * design_region_size.y, +0.5 * design_region_size.y, ny)
+    xv, yv = np.meshgrid(xcoord, ycoord, indexing="ij")
 
     return xv, yv
 
 
-def signed_distance(weights: anp.ndarray) -> anp.ndarray:
-    """Maps the 2D weights using a signed-distance function."""
-
-    # Create signed distance function.
-    sd = skfmm.distance(weights - 0.5)
-
-    # Linear interpolation of zero-levelset onto 0.5-levelset.
-    xp = [anp.min(sd.flatten()), 0, anp.max(sd.flatten())]
-    yp = [0, 0.5001, 1]
-
-    return anp.interp(sd.flatten(), xp, yp)
-
-
 @primitive
-def levelset_and_smoothing(
-    grating_height_um: float,
-    grating_duty_cycle: float,
-    design_region_size: mp.Vector3,
-    nx: int,
-    ny: int,
-) -> anp.ndarray:
-    """Generates the grating as a levelset with signed-distance smoothing."""
+def levelset_and_smoothing(grating_height_um: float) -> np.ndarray:
+    """Returns the density weights for a binary grating as a levelset.
 
-    xv, yv = design_region_to_meshgrid(design_region_size, nx, ny)
+    Args:
+      grating_height_um: the height of the grating.
 
-    weights = anp.where(
-        anp.abs(yv) <= 0.5 * grating_duty_cycle * GRATING_PERIOD_UM,
-        anp.where(xv <= xv[0][0] + grating_height_um, 1, 0),
+    Returns:
+      The density weights as a flattened (1D) array.
+    """
+
+    xv, yv = design_region_to_meshgrid(nx_design_grid, ny_design_grid)
+    weights = np.where(
+        np.abs(yv) <= 0.5 * GRATING_DUTY_CYCLE * GRATING_PERIOD_UM,
+        np.where(xv <= xv[0][0] + grating_height_um, 1, 0),
         0,
     )
+    xcoord = np.linspace(
+        -0.5 * design_region_size.x, +0.5 * design_region_size.x, nx_design_grid
+    )
+    ycoord = np.linspace(
+        -0.5 * design_region_size.y, +0.5 * design_region_size.y, ny_design_grid
+    )
+    interp = RegularGridInterpolator((xcoord, ycoord), weights)
 
-    smoothed_weights = signed_distance(weights)
+    # Smooth the design weights by downsampling from the design grid
+    # to the simulation grid using bilinear interpolation.
+    sim_grid_xv, sim_grid_yv = design_region_to_meshgrid(nx_sim_grid, ny_sim_grid)
+    smoothed_weights = interp((sim_grid_xv, sim_grid_yv))
 
-    return smoothed_weights
+    if DEBUG_OUTPUT:
+        fig, ax = plt.subplots()
+        im = ax.imshow(
+            np.transpose(smoothed_weights),
+            cmap="binary",
+            interpolation="none",
+            aspect="equal",
+        )
+        ax.set_title(r"$\rho_{smooth-levelset}(h)$")
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        fig.colorbar(im, cax=cax)
+        if mp.am_master():
+            fig.savefig("smoothed_levelset.png", dpi=150, bbox_inches="tight")
+
+    return smoothed_weights.flatten()
 
 
 def levelset_and_smoothing_vjp(
-    ans: anp.ndarray,
+    ans: np.ndarray,
     grating_height_um: float,
-    grating_duty_cycle: float,
-    design_region_size: mp.Vector3,
-    nx: int,
-    ny: int,
-) -> anp.ndarray:
-    """Computes the vector-Jacobian product."""
+) -> Callable[[np.ndarray], np.ndarray]:
+    """Returns a function for computing the vector-Jacobian product.
 
-    xv, yv = design_region_to_meshgrid(design_region_size, nx, ny)
+       The Jacobian is computed manually using a finite difference.
 
-    # pixel dimensions
-    delta_x = design_region_size.x / (nx - 1)
+    Args:
+      ans: the design weights for the smoothed grating (no perturbation).
+      grating_height_um: the height of the grating.
 
-    weights = anp.where(
-        anp.abs(yv) <= 0.5 * grating_duty_cycle * GRATING_PERIOD_UM,
-        anp.where(xv <= xv[0][0] + grating_height_um + delta_x, 1, 0),
+    Returns:
+      An anonymous function for computing the vector-Jacobian product.
+    """
+
+    xv, yv = design_region_to_meshgrid(nx_design_grid, ny_design_grid)
+    weights = np.where(
+        np.abs(yv) <= 0.5 * GRATING_DUTY_CYCLE * GRATING_PERIOD_UM,
+        np.where(
+            xv <= xv[0][0] + grating_height_um + GRATING_HEIGHT_PERTURBATION_UM, 1, 0
+        ),
         0,
     )
-
-    jacobian = (signed_distance(weights) - ans) / delta_x
-    print(f"jacobian:, {anp.linalg.norm(jacobian)}")
-
-    extent = [xv[0][0], xv[0][-1], yv[0][0], yv[0][-1]]
-    fig, ax = plt.subplots()
-    im = ax.imshow(
-        anp.transpose(jacobian.reshape(nx, ny)),
-        extent=extent,
-        cmap="hot_r",
-        interpolation="none",
-        aspect="equal",
+    xcoord = np.linspace(
+        -0.5 * design_region_size.x, +0.5 * design_region_size.x, nx_design_grid
     )
-    ax.set_title("gradient")
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    fig.colorbar(im, cax=cax)
-    if mp.am_master():
-        fig.savefig("grating_gradient.png", dpi=150, bbox_inches="tight")
+    ycoord = np.linspace(
+        -0.5 * design_region_size.y, +0.5 * design_region_size.y, ny_design_grid
+    )
+    interp = RegularGridInterpolator((xcoord, ycoord), weights)
 
-    return lambda g: anp.tensordot(g, jacobian, axes=1)
+    # Smooth the design weights by downsampling from the design grid
+    # to the simulation grid using bilinear interpolation.
+    xv_sim_grid, yv_sim_grid = design_region_to_meshgrid(nx_sim_grid, ny_sim_grid)
+    smoothed_weights = interp((xv_sim_grid, yv_sim_grid))
+    smoothed_weights = smoothed_weights.flatten()
+
+    jacobian = (smoothed_weights - ans) / GRATING_HEIGHT_PERTURBATION_UM
+
+    if DEBUG_OUTPUT:
+        fig, ax = plt.subplots()
+        im = ax.imshow(
+            np.transpose(jacobian.reshape(nx_sim_grid, ny_sim_grid)),
+            cmap="inferno",
+            interpolation="none",
+            aspect="equal",
+        )
+        ax.set_title(r"$\partial \rho_{smooth-levelset} / \partial h$")
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        fig.colorbar(im, cax=cax)
+        if mp.am_master():
+            fig.savefig("gradient_smooth_levelset.png", dpi=150, bbox_inches="tight")
+
+    return lambda g: np.tensordot(g, jacobian, axes=1)
 
 
-def grating_1d(
-    pol: Polarization,
-    grating_height_um: float,
-    design_region_size: mp.Vector3,
-    nx: int,
-    ny: int,
-) -> mpa.OptimizationProblem:
-    """Sets up the adjoint optimization of a 1D grating."""
+def grating_1d(pol: Polarization) -> mpa.OptimizationProblem:
+    """Sets up the adjoint optimization of a 1D grating.
+
+    Args:
+      pol: the polarization state (S or P).
+
+    Returns:
+      A meep.adjoint.OptimizationProblem object for the simulation.
+    """
 
     frequency = 1 / WAVELENGTH_UM
-    substrate_um = 2.0
-    pml_um = 1.0
-    pml_layers = [mp.PML(direction=mp.X, thickness=pml_um)]
-
-    n_glass = 1.5
-    glass = mp.Medium(index=n_glass)
-
-    size_x_um = pml_um + substrate_um + grating_height_um + PADDING_UM + pml_um
+    pml_layers = [mp.PML(direction=mp.X, thickness=PML_UM)]
+    glass = mp.Medium(index=N_GLASS)
+    size_x_um = (
+        PML_UM
+        + SUBSTRATE_UM
+        + GRATING_HEIGHT_UM
+        + GRATING_HEIGHT_PERTURBATION_UM
+        + AIR_UM
+        + PML_UM
+    )
     size_y_um = GRATING_PERIOD_UM
     cell_size = mp.Vector3(size_x_um, size_y_um, 0)
-
     k_point = mp.Vector3()
 
     if pol.name == "S":
@@ -150,7 +202,7 @@ def grating_1d(
         eig_parity = mp.EVEN_Z
         src_cmpt = mp.Hz
 
-    src_pt = mp.Vector3(-0.5 * size_x_um + pml_um, 0, 0)
+    src_pt = mp.Vector3(-0.5 * size_x_um + PML_UM, 0, 0)
     sources = [
         mp.Source(
             mp.GaussianSource(frequency, fwidth=0.1 * frequency),
@@ -161,19 +213,18 @@ def grating_1d(
     ]
 
     matgrid = mp.MaterialGrid(
-        mp.Vector3(nx, ny),
+        mp.Vector3(nx_sim_grid, ny_sim_grid),
         mp.air,
         glass,
-        weights=anp.ones((nx, ny)),
-        do_averaging=True,
-        beta=anp.inf,
+        weights=np.ones((nx_sim_grid, ny_sim_grid)),
+        do_averaging=False,
     )
 
     matgrid_region = mpa.DesignRegion(
         matgrid,
         volume=mp.Volume(
             center=mp.Vector3(
-                (-0.5 * size_x_um + pml_um + substrate_um + 0.5 * design_region_size.x),
+                (-0.5 * size_x_um + PML_UM + SUBSTRATE_UM + 0.5 * design_region_size.x),
                 0,
                 0,
             ),
@@ -184,8 +235,8 @@ def grating_1d(
     geometry = [
         mp.Block(
             material=glass,
-            size=mp.Vector3(pml_um + substrate_um, mp.inf, mp.inf),
-            center=mp.Vector3(-0.5 * size_x_um + 0.5 * (pml_um + substrate_um), 0, 0),
+            size=mp.Vector3(PML_UM + SUBSTRATE_UM, mp.inf, mp.inf),
+            center=mp.Vector3(-0.5 * size_x_um + 0.5 * (PML_UM + SUBSTRATE_UM), 0, 0),
         ),
         mp.Block(
             material=matgrid,
@@ -203,15 +254,13 @@ def grating_1d(
         geometry=geometry,
     )
 
-    tran_pt = mp.Vector3(0.5 * size_x_um - pml_um, 0, 0)
+    tran_pt = mp.Vector3(0.5 * size_x_um - PML_UM, 0, 0)
 
-    order_y = 1
     kdiff = mp.Vector3(
-        (frequency**2 - (order_y / GRATING_PERIOD_UM) ** 2) ** 0.5,
-        order_y / GRATING_PERIOD_UM,
+        (frequency**2 - (DIFFRACTION_ORDER / GRATING_PERIOD_UM) ** 2) ** 0.5,
+        DIFFRACTION_ORDER / GRATING_PERIOD_UM,
         0,
     )
-    print(f"kdiff = ({kdiff.x:.5f}, {kdiff.y:.5f}, {kdiff.z:.5f})")
 
     obj_args = [
         mpa.EigenmodeCoefficient(
@@ -227,8 +276,8 @@ def grating_1d(
         ),
     ]
 
-    def obj_func(tran_mon):
-        return anp.abs(tran_mon) ** 2
+    def obj_func(mode_coeff):
+        return npa.abs(mode_coeff) ** 2
 
     opt = mpa.OptimizationProblem(
         simulation=sim,
@@ -242,70 +291,56 @@ def grating_1d(
 
 
 if __name__ == "__main__":
-    grating_height_um = 0.52
-    grating_duty_cycle = 0.65
-    height_perturbation_um = 2.0 / RESOLUTION_UM
+    # Only the S polarization is supported (for now).
+    opt = grating_1d(Polarization.S)
 
-    design_region_size = mp.Vector3(
-        grating_height_um + PADDING_UM, GRATING_PERIOD_UM, 0
-    )
-    design_region_resolution_um = int(2 * RESOLUTION_UM)
-    nx = int(design_region_size.x * design_region_resolution_um) + 1
-    ny = int(design_region_size.y * design_region_resolution_um) + 1
+    smoothed_design_weights = levelset_and_smoothing(GRATING_HEIGHT_UM)
 
-    opt = grating_1d(
-        Polarization.P,
-        grating_height_um,
-        design_region_size,
-        nx,
-        ny,
+    obj_val_unperturbed, grad_unperturbed = opt(
+        [smoothed_design_weights], need_gradient=True
     )
 
-    smoothed_design_weights = levelset_and_smoothing(
-        grating_height_um,
-        grating_duty_cycle,
-        design_region_size,
-        nx,
-        ny,
-    )
+    if DEBUG_OUTPUT:
+        fig, ax = plt.subplots()
+        im = ax.imshow(
+            np.transpose(grad_unperturbed.reshape(nx_sim_grid, ny_sim_grid)),
+            cmap="inferno",
+            interpolation="none",
+            aspect="equal",
+        )
+        ax.set_title(r"$\partial F / \partial \rho_{smooth-levelset}$")
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        fig.colorbar(im, cax=cax)
+        if mp.am_master():
+            fig.savefig(
+                "gradient_wrt_smoothed_design_weights.png", dpi=150, bbox_inches="tight"
+            )
 
-    obj_value_unperturbed, grad_unperturbed = opt(
-        [smoothed_design_weights],
-        need_gradient=True,
-    )
-
-    fig, ax = plt.subplots()
-    opt.plot2D(init_opt=False, ax=ax)
-    if mp.am_master():
-        fig.savefig("grating_1D_plot2D.png", dpi=150, bbox_inches="tight")
+        fig, ax = plt.subplots()
+        opt.plot2D(init_opt=False, ax=ax)
+        if mp.am_master():
+            fig.savefig("sim_layout.png", dpi=150, bbox_inches="tight")
 
     defvjp(levelset_and_smoothing, levelset_and_smoothing_vjp)
     grad_backprop = tensor_jacobian_product(levelset_and_smoothing, 0)(
-        grating_height_um,
-        grating_duty_cycle,
-        design_region_size,
-        nx,
-        ny,
+        GRATING_HEIGHT_UM,
         grad_unperturbed,
     )
 
     perturbed_design_weights = levelset_and_smoothing(
-        grating_height_um + height_perturbation_um,
-        grating_duty_cycle,
-        design_region_size,
-        nx,
-        ny,
+        GRATING_HEIGHT_UM + GRATING_HEIGHT_PERTURBATION_UM
     )
     perturbed_design_weights = perturbed_design_weights.flatten()
 
-    obj_value_perturbed, _ = opt([perturbed_design_weights], need_gradient=False)
+    obj_val_perturbed, _ = opt([perturbed_design_weights], need_gradient=False)
 
-    adj_directional_deriv = height_perturbation_um * grad_backprop
-    fnd_directional_deriv = obj_value_perturbed[0] - obj_value_unperturbed[0]
+    adj_directional_deriv = GRATING_HEIGHT_PERTURBATION_UM * grad_backprop
+    fnd_directional_deriv = obj_val_perturbed[0] - obj_val_unperturbed[0]
     rel_err = abs(
         (fnd_directional_deriv - adj_directional_deriv) / fnd_directional_deriv
     )
     print(
-        f"directional-deriv:, {fnd_directional_deriv:.8f} (finite difference), "
+        f"dir-deriv:, {fnd_directional_deriv:.8f} (finite difference), "
         f"{adj_directional_deriv:.8f} (adjoint), {rel_err:.6f} (error)"
     )
