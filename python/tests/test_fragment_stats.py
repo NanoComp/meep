96c52b00998fcd7eba43a779d8943c04e7514703
Ardavan Oskooi <ardavan.oskooi@gmail.com>
2025-01-26 17:55:46 -0800

    fixes for test_fragment_stats.py

--- python/tests/test_fragment_stats.py
+++ python/tests/test_fragment_stats.py
@@ -24,7 +24,7 @@ def make_dft_vecs(
 
 
 def make_sim(cell, res, pml, dims, create_gv=True, k_point=False):
-    if mp.count_processors() == 2 and dims == 3:
+    if mp.count_processors() == 2:
         chunk_layout = mp.BinaryPartition(data=[(mp.X, 0), 0, 1])
     else:
         chunk_layout = None
