ceff208a29dd69a84e48a718f2a01a84cc73e0a0
Steven G. Johnson <stevenj@alum.mit.edu>
2026-03-07 18:57:18 -0500

    add force symmetry test

--- python/tests/test_force.py
+++ python/tests/test_force.py
@@ -5,50 +5,93 @@ import meep as mp
 
 class TestForce(unittest.TestCase):
     def setUp(self):
+        self.resolution = 20
+        self.cell = mp.Vector3(10, 10)
+        self.pml_layers = mp.PML(1.0)
 
-        resolution = 20
-        cell = mp.Vector3(10, 10)
-        pml_layers = mp.PML(1.0)
-        fcen = 1.0
-        df = 1.0
-        sources = mp.Source(
-            src=mp.GaussianSource(fcen, fwidth=df), center=mp.Vector3(), component=mp.Ez
+        self.fcen = 1.0
+        self.df = 1.0
+        self.sources = mp.Source(
+            src=mp.GaussianSource(frequency=self.fcen, fwidth=self.df),
+            center=mp.Vector3(),
+            component=mp.Ez,
         )
 
-        self.sim = mp.Simulation(
-            resolution=resolution,
-            cell_size=cell,
-            boundary_layers=[pml_layers],
-            sources=[sources],
-        )
-
-        fr = mp.ForceRegion(mp.Vector3(y=1.27), direction=mp.Y, size=mp.Vector3(4.38))
-        self.myforce = self.sim.add_force(fcen, 0, 1, fr, decimation_factor=1)
-        self.myforce_decimated = self.sim.add_force(
-            fcen, 0, 1, fr, decimation_factor=10
+        self.fr = mp.ForceRegion(
+            center=mp.Vector3(0, 1.27), direction=mp.Y, size=mp.Vector3(4.38, 0)
         )
 
     def test_force(self):
+        """Test force calculations using a golden master."""
 
-        self.sim.run(
+        sim = mp.Simulation(
+            resolution=self.resolution,
+            cell_size=self.cell,
+            boundary_layers=[self.pml_layers],
+            sources=[self.sources],
+        )
+
+        force = sim.add_force(self.fcen, 0, 1, self.fr, decimation_factor=1)
+        force_decimated = sim.add_force(self.fcen, 0, 1, self.fr, decimation_factor=10)
+
+        sim.run(
             until_after_sources=mp.stop_when_fields_decayed(
                 50, mp.Ez, mp.Vector3(), 1e-6
             )
         )
 
-        # Test store and load of force as numpy array
-        fdata = self.sim.get_force_data(self.myforce)
-        self.sim.load_force_data(self.myforce, fdata)
-
-        self.sim.display_forces(self.myforce)
-        f = mp.get_forces(self.myforce)
+        sim.display_forces(force)
+        f = mp.get_forces(force)
+        f_decimated = mp.get_forces(force_decimated)
 
         self.assertAlmostEqual(f[0], -0.11039089113393187)
 
         places = 6 if mp.is_single_precision() else 7
-        self.assertAlmostEqual(
-            f[0], mp.get_forces(self.myforce_decimated)[0], places=places
+        self.assertAlmostEqual(f[0], f_decimated[0], places=places)
+
+        # Test store and load of force as numpy array
+        fdata = sim.get_force_data(force)
+        sim.load_force_data(force, fdata)
+        f_fdata = mp.get_forces(force)
+        self.assertAlmostEqual(f_fdata[0], f[0], places=7)
+
+    def test_force_with_symmetry(self):
+        """Test that force calculations produce correct results with mirror symmetry."""
+
+        # Run without mirror symmetry.
+        sim_nosym = mp.Simulation(
+            resolution=self.resolution,
+            cell_size=self.cell,
+            boundary_layers=[self.pml_layers],
+            sources=[self.sources],
         )
+
+        force_nosym = sim_nosym.add_force(self.fcen, 0, 1, self.fr)
+        sim_nosym.run(
+            until_after_sources=mp.stop_when_fields_decayed(
+                50, mp.Ez, mp.Vector3(), 1e-6
+            )
+        )
+        f_nosym = mp.get_forces(force_nosym)
+
+        # Run with mirror symmetry.
+        sim_sym = mp.Simulation(
+            resolution=self.resolution,
+            cell_size=self.cell,
+            boundary_layers=[self.pml_layers],
+            sources=[self.sources],
+            symmetries=[mp.Mirror(mp.Y)],
+        )
+
+        force_sym = sim_sym.add_force(self.fcen, 0, 1, self.fr)
+        sim_sym.run(
+            until_after_sources=mp.stop_when_fields_decayed(
+                50, mp.Ez, mp.Vector3(), 1e-6
+            )
+        )
+        f_sym = mp.get_forces(force_sym)
+
+        self.assertAlmostEqual(f_nosym[0], f_sym[0], places=7)
 
 
 if __name__ == "__main__":
